72d3c5524a585d7f7268e5a0be43aba8c3ee0c83
anthonyjchriste <achriste@hawaii.edu>
2017-09-15 10:16:59 -1000

    Make the mauka-cli available

---
+++ util/mauka/mauka-cli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /usr/local/bin/opq/mauka
+python3 -m plugins.manager /etc/opq/mauka/config.json
--- util/mauka/mauka-install.sh
+++ util/mauka/mauka-install.sh
@@ -10,6 +10,9 @@ set -o xtrace
 cp mauka-service.sh /etc/init.d/mauka
 update-rc.d mauka defaults
 
+cp mauka-cli.sh /usr/local/bin/mauka-cli
+chmod +x /usr/local/bin/mauka-cli
+
 cd ../..
 cp -R mauka /usr/local/bin/opq/.
 cp mauka/config.json /etc/opq/mauka/config.json
