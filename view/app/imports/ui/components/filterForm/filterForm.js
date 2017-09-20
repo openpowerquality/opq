c250ddd27cf91da5bc1217c1597019168110846e
David Aghalarpour <aghalarp@hawaii.edu>
2017-09-20 12:34:37 -1000

    Implement filter form validation and add datetime picker widget.

--- view/app/imports/ui/components/filterForm/filterForm.js
+++ view/app/imports/ui/components/filterForm/filterForm.js
@@ -1,12 +1,14 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import {colorQuantify, dataContextValidator, jQueryPromise} from '../../../utils/utils';
+import {colorQuantify, dataContextValidator, jQueryPromise, timeUnitString} from '../../../utils/utils.js';
 import './filterForm.html';
 import '../form-controls/text-form-control.html'
 import flatpickr from 'flatpickr';
 import '../../../../node_modules/flatpickr/dist/flatpickr.min.css';
-import { BoxEvents } from '../../../api/boxEvent/BoxEventCollection';
+import { boxEventsCountMap } from '../../../api/boxEvent/BoxEventCollectionMethods.js';
+import { mapify } from 'es6-mapify';
 import Moment from 'moment';
+import { filterFormSchema } from '../../../utils/schemas.js'
 
 Template.Filter_Form.onCreated(function() {
   const template = this;
@@ -26,81 +28,99 @@ Template.Filter_Form.onCreated(function() {
   template.autorun(() => {
     const dataContext = Template.currentData();
     template.filterSource = (dataContext.filterSource) ? dataContext.filterSource : null;
-    console.log('filterSource: ', template.filterSource);
-
   });
 
-  // Form Validation fields
-  //template.validationContext = signupPageSchema.namedContext('Filter_Form');
-  //template.formSubmissionErrors = new ReactiveVar();
+  // Form Validation Context
+  template.validationContext = filterFormSchema.namedContext('Filter_Form');
 
   // Holds the timestamp of the beginning of month. Initial value is the current month.
   template.dayPickerCurrentMonth = new ReactiveVar(Moment().startOf('month').toDate());
 
-  // Subscription
+  // Holds most recent event count map/dict.
+  template.currentBoxEventsCountMap = new ReactiveVar();
+
+  // When month changes, retrieve event count for that month. This data will be inserted to dayPicker widget.
   template.autorun(() => {
     const currentMonth = template.dayPickerCurrentMonth.get();
-
     if (currentMonth) {
       const endOfMonth = Moment(currentMonth).endOf('month').toDate();
-      console.log('currentMonth: ', currentMonth);
-      console.log('endOfMonth: ', endOfMonth);
-      template.subscribe(BoxEvents.publicationNames.GET_BOX_EVENTS, {startTime: currentMonth, endTime: endOfMonth});
+      boxEventsCountMap.call({timeUnit: 'dayOfMonth', startTime: currentMonth, stopTime: endOfMonth}, (error, eventCountMap) => {
+        if (error) {
+          console.log(error)
+        } else {
+          const ecm = mapify(eventCountMap); // Had to be demapified (aka serialized) before being sent from server.
+          template.currentBoxEventsCountMap.set(ecm);
+        }
+      });
     }
-
   });
 
 });
 
 Template.Filter_Form.onRendered(function() {
   const template = this;
+  const dataContext = template.data;
 
-  // Instantiate flatpickr widget, attach to template.
-  template.autorun(() => {
-    const currentMonth = template.dayPickerCurrentMonth.get();
+  // Instantiate flatpickr for startTime and endTime
+  if (dataContext.timeInterval) {
+    const flatpickrConfig = {
+      enableTime: true,
+      enableSeconds: true
+    };
+    template.startTimeFlatpickr = flatpickr('#startTime', flatpickrConfig);
+    template.endTimeFlatpickr = flatpickr('#endTime', flatpickrConfig);
+  }
 
-    if (currentMonth && template.subscriptionsReady()) {
-      const flatpickrConfig = {
-        onDayCreate: function(dObj, dStr, fp, dayElem) {
-          const computation = template.autorun(() => {
-            const currDate = dayElem.dateObj;
-            // Ignore the prevMonthDays and nextMonthDays on calendar.
-            if (currDate.getMonth() === fp.currentMonth) {
-              const currentMonth = template.dayPickerCurrentMonth.get();
-              // console.log('cm new: ', fp.currentMonth, currentMonth, Tracker.currentComputation);
+  // Instantiate flatpickr widget for dayPicker.
+  // Config here is a bit complicated - but serves as a proof of concept on how to reactively associate data with the
+  // flatpickr widget.
+  if (dataContext.dayPicker) {
+    template.autorun(() => {
+      const currentMonth = template.dayPickerCurrentMonth.get();
+      const eventsCountMap = template.currentBoxEventsCountMap.get();
 
-              const endOfDay = Moment(currDate).endOf('day').toDate();
-              const selector = BoxEvents.queryConstructors().getBoxEvents({startTime: currDate, endTime: endOfDay});
-              const dailyEventsCount = BoxEvents.find(selector, {}).count();
+      if (currentMonth && eventsCountMap && template.subscriptionsReady()) {
+        const flatpickrConfig = {
+          onDayCreate: function(dObj, dStr, fp, dayElem) {
+            const computation = template.autorun(() => {
+              const currDate = dayElem.dateObj;
+              const currMonth = Moment(currDate).month(); // 0-11
+              const timeUnitKey = timeUnitString(currDate, 'dayOfMonth');
 
-              if (dailyEventsCount && template.subscriptionsReady()) {
-                const bgColor = colorQuantify(dailyEventsCount, [1, 50], ['#00ff00', '#ffff00', '#ff0000']);
-                $(dayElem).append(`<span class="flatpickr-day-highlight" style="background: ${bgColor};"></span>`);
+              // Ignore the prevMonthDays and nextMonthDays on calendar.
+              if (currMonth === fp.currentMonth) {
+                const eventsCountMap = template.currentBoxEventsCountMap.get();
+                const eventCount = eventsCountMap.get(timeUnitKey);
+
+                if (eventsCountMap && eventCount && template.subscriptionsReady()) {
+                  $(dayElem).attr('data-tooltip', `${eventCount} events`);
+                  const bgColor = colorQuantify(eventCount, 1, 50, ['#00ff00', '#ffff00', '#ff0000']);
+                  $(dayElem).append(`<span class="flatpickr-day-highlight" style="background: ${bgColor};"></span>`);
+                }
               }
-            }
-          });
+            });
 
-          (fp.trackerComputations) ? fp.trackerComputations.push(computation) : fp.trackerComputations = [computation];
-        },
-        onMonthChange: function(selectedDates, dateStr, fpInstance) {
-          // Subscribe to month event per day
-          console.log('monthChange');
-          // $('.flatpickr-day.nextMonthDay span.flatpickr-day-highlight').remove();
-          // $('.flatpickr-day.prevMonthDay span.flatpickr-day-highlight').remove();
-          const oldComputations = fpInstance.trackerComputations.splice(0,42);
-          oldComputations.forEach(comp => comp.stop());
-          // console.log('computations: ', fpInstance.trackerComputations);
-          // console.log(selectedDates, dateStr, fpInstance);
-          // Get the Date obj representing start of the current month.
-          const date = Moment().month(fpInstance.currentMonth).year(fpInstance.currentYear).startOf('month').toDate();
-          template.dayPickerCurrentMonth.set(date);
-        }
-      };
+            (fp.trackerComputations) ? fp.trackerComputations.push(computation) : fp.trackerComputations = [computation];
+          },
+          onMonthChange: function(selectedDates, dateStr, fpInstance) {
+            // Note: It seems this callback is fired AFTER onDayCreate() when we change month.
 
-      // Only instantiate once.
-      if (!template.dayPicker) template.dayPicker = flatpickr('#dayPicker', flatpickrConfig);
-    }
-  });
+            // Remove old tracker computations. This is important, otherwise we will end up creating a ton of autoruns.
+            const oldComputations = fpInstance.trackerComputations.splice(0,42);
+            oldComputations.forEach(comp => comp.stop());
+
+            // Set RV to new month (which will trigger autorun to method call for new event counts).
+            const date = Moment().month(fpInstance.currentMonth).year(fpInstance.currentYear).startOf('month').toDate();
+            template.dayPickerCurrentMonth.set(date);
+          }
+        };
+
+        // Only instantiate once.
+        if (!template.dayPicker) template.dayPicker = flatpickr('#dayPicker', flatpickrConfig);
+      }
+    });
+  }
+
 
 });
 
@@ -110,25 +130,26 @@ Template.Filter_Form.events({
     const dataContext = template.data; // Non-reactive
 
     const formData = {};
-    if (dataContext.frequencyRange) {
-      formData.minFrequency = event.target.minFrequency.value;
-      formData.maxFrequency = event.target.maxFrequency.value;
-    }
-    if (dataContext.voltageRange) {
-      formData.minVoltage = event.target.minVoltage.value;
-      formData.maxVoltage = event.target.maxVoltage.value;
-    }
-    // if (dataContext.durationRange) {
-      formData.minDuration = event.target.minDuration.value;
-      formData.maxDuration = event.target.maxDuration.value;
-    // }
-    if (dataContext.timeInterval) {
-      formData.startTime = event.target.startTime.value;
-      formData.endTime = event.target.endTime.value;
-    }
+    if (event.target.minFrequency) formData.minFrequency = event.target.minFrequency.value;
+    if (event.target.maxFrequency) formData.maxFrequency = event.target.maxFrequency.value;
 
-    console.log('Filter_Form formData submit event: ', formData);
+    if (event.target.minVoltage) formData.minVoltage = event.target.minVoltage.value;
+    if (event.target.maxVoltage) formData.maxVoltage = event.target.maxVoltage.value;
 
-    template.filterSource.set(formData);
+    if (event.target.minDuration) formData.minDuration = event.target.minDuration.value;
+    if (event.target.maxDuration) formData.maxDuration = event.target.maxDuration.value;
+
+    if (event.target.startTime) formData.startTime = event.target.startTime.value;
+    if (event.target.endTime) formData.endTime = event.target.endTime.value;
+
+    if (event.target.dayPicker) formData.dayPicker = event.target.dayPicker.value;
+
+    // Clean and validate form data.
+    template.validationContext.resetValidation();
+    filterFormSchema.clean(formData);
+    template.validationContext.validate(formData);
+
+    // Set ReactiveVar if valid.
+    if (template.validationContext.isValid()) template.filterSource.set(formData);
   }
 });
