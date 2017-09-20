9531e6aac783874ddcc659ac127da22f542bb688
Serge <sin8@hawaii.edu>
2017-09-19 16:57:51 -1000

    Made all field symbolic constants. Fixed the Box ID fielt to mongo_box_id

--- makai/AcquisitionBroker/src/MongoDriver.cc
+++ makai/AcquisitionBroker/src/MongoDriver.cc
@@ -16,6 +16,18 @@ using bsoncxx::builder::stream::open_document;
 static const std::string OPQ_DB = "opq";
 static const std::string OPQ_EVENT_COLLECTION = "events";
 static const std::string OPQ_DATA_COLLECTION = "data";
+static const std::string BOX_ID_FIELD = "mongo_box_id";
+static const std::string EVENT_NUMBER_FIELD = "event_number";
+static const std::string EVENT_TYPE_FIELD = "type";
+static const std::string DESCRIPTION_FIELD = "description";
+static const std::string BOXES_TRIGGERED_FIELD = "boxes_triggered";
+static const std::string BOXES_RECEIVED_FIELD = "boxes_received";
+static const std::string EVENT_START_FIELD = "event_start";
+static const std::string EVENT_END_FIELD = "event_end";
+static const std::string TIME_STAMP_FIELD = "time_stamp";
+static const std::string TIME_DATA_FIELD = "data";
+
+
 
 MongoDriver::MongoDriver(std::string uri) : _client(mongocxx::uri(uri)){
     _db = _client[OPQ_DB];
@@ -30,28 +42,28 @@ bool MongoDriver::init_mongo_client() {
 uint32_t MongoDriver::get_next_event_number() {
     mongocxx::options::find opts;
     opts.limit( 1 );
-    opts.sort(document{} << "event_number" << -1 << finalize);
+    opts.sort(document{} << EVENT_NUMBER_FIELD << -1 << finalize);
     auto result = _event_collection.find_one(document{}.view(), opts);
     if (!result) return 1;
-    int32_t next_event_number = result->view()["event_number"].get_int32() + 1;
+    int32_t next_event_number = result->view()[EVENT_NUMBER_FIELD].get_int32() + 1;
     if(next_event_number < 0) return 1;
     else return next_event_number;
 }
 
 bool MongoDriver::create_event(opq::proto::RequestEventMessage &m, uint64_t ts, uint32_t event_num) {
     auto builder = document{};
-    builder << "type" << opq::proto::RequestEventMessage_TriggerType_Name(m.trigger_type())
-            << "event_number" << (int32_t)event_num
-            << "description" << m.description();
+    builder << EVENT_TYPE_FIELD << opq::proto::RequestEventMessage_TriggerType_Name(m.trigger_type())
+            << EVENT_NUMBER_FIELD << (int32_t)event_num
+            << DESCRIPTION_FIELD << m.description();
 
-    auto box_array = builder << "boxes_triggered" << open_array;
+    auto box_array = builder << BOXES_TRIGGERED_FIELD << open_array;
     std::for_each(m.box_ids().begin(), m.box_ids().end(),
                   [&box_array](int box){box_array << box;});
 
     box_array << close_array;
-    builder << "boxes_recieved" << open_array << close_array;
-    builder << "event_start" << (int64_t)m.start_timestamp_ms_utc()
-            << "event_end" << (int64_t)m.end_timestamp_ms_utc();
+    builder << BOXES_RECEIVED_FIELD << open_array << close_array;
+    builder << EVENT_START_FIELD << (int64_t)m.start_timestamp_ms_utc()
+            << EVENT_END_FIELD << (int64_t)m.end_timestamp_ms_utc();
     bsoncxx::document::value doc_value = builder << finalize;
 
     auto result = _event_collection.insert_one(doc_value.view());
@@ -62,13 +74,13 @@ bool MongoDriver::append_data_to_event(std::vector<opq::proto::DataMessage> &mes
         return false;
     int32_t id = messages.front().id();
     _event_collection.update_one(document{}
-                                         << "event_number" << (int32_t)event_num
+                                         << EVENT_NUMBER_FIELD << (int32_t)event_num
                                          << finalize,
                                  document{}
                                          << "$push"
                                          << open_document
-                                               <<"boxes_recieved" << id
-                                               <<"time_stamp" << (int64_t)chrono_to_mili_now()
+                                               <<BOXES_RECEIVED_FIELD << id
+                                               <<TIME_STAMP_FIELD << (int64_t)chrono_to_mili_now()
                                          << close_document
                                          << finalize);
     auto builder = bsoncxx::builder::stream::document{};
@@ -76,11 +88,11 @@ bool MongoDriver::append_data_to_event(std::vector<opq::proto::DataMessage> &mes
     size_t cycle_size = (size_t)messages.front().cycles().size();
     auto end_time = messages.front().cycles().Get(cycle_size -1).time();
 
-    builder << "Box ID" << id
-            << "time_start" << (int64_t)start_time
-            << "time_end" << (int64_t)end_time
-	    << "event_number" << (int32_t)event_num;
-    auto time_array_context = builder << "time_stamps" << bsoncxx::builder::stream::open_array;
+    builder << BOX_ID_FIELD << id
+            << EVENT_START_FIELD << (int64_t)start_time
+            << EVENT_END_FIELD << (int64_t)end_time
+	    << EVENT_NUMBER_FIELD << (int32_t)event_num;
+    auto time_array_context = builder << TIME_STAMP_FIELD << bsoncxx::builder::stream::open_array;
     for (auto &message : messages){
         for(auto &cycle : message.cycles()) {
             time_array_context << (int64_t)cycle.time();
@@ -88,7 +100,7 @@ bool MongoDriver::append_data_to_event(std::vector<opq::proto::DataMessage> &mes
     }
     time_array_context << close_array;
 
-    auto data_array_context = builder << "data" << bsoncxx::builder::stream::open_array;
+    auto data_array_context = builder << TIME_DATA_FIELD << bsoncxx::builder::stream::open_array;
     for (auto &message : messages){
         for(auto &cycle : message.cycles()) {
             std::for_each(cycle.data().begin(), cycle.data().end(),
