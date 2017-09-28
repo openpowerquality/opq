7911174a39187526a391528f93a26878423e7c9e
David Aghalarpour <aghalarp@hawaii.edu>
2017-09-28 05:45:08 -1000

    Add EventMetaData collection, methods, and publications.

---
+++ view/app/imports/api/eventMetaData/index.js
@@ -0,0 +1,2 @@
+import './EventMetaDataCollection.js';
+import './EventMetaDataCollectionMethods.js';
--- view/app/imports/startup/server/publications.js
+++ view/app/imports/startup/server/publications.js
@@ -1,9 +1,11 @@
 import { Measurements } from '../../api/measurement/MeasurementCollection.js';
 import { BoxEvents } from '../../api/boxEvent/BoxEventCollection.js';
 import { EventData } from '../../api/eventData/EventDataCollection.js';
+import { EventMetaData } from '../../api/eventMetaData/EventMetaDataCollection';
 import '../../api/simulatedEvents/simulatedEventsPublications.js';
 
 
 Measurements.publish();
 BoxEvents.publish();
 EventData.publish();
+EventMetaData.publish();
