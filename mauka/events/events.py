c32dcea809136a192f5d51082df894a47e5713d4
anthonyjchriste <achriste@hawaii.edu>
2017-10-18 12:40:05 -1000

    mauka: Pass mongo client into fs reader so a new instance is created everytime a new fule needed read

--- mauka/events/events.py
+++ mauka/events/events.py
@@ -15,17 +15,17 @@ def get_data(mongo_client, event_data):
         print("No data pointer in event metadata")
 
 
-def load_event(event_id: int):
-    mongo_client = mongo.mongo.OpqMongoClient()
+def load_event(event_id: int, mongo_client=None):
+    _mongo_client = mongo_client if mongo_client is not None else mongo.mongo.OpqMongoClient()
 
-    events_collection = mongo_client.get_collection(mongo.mongo.Collection.EVENTS)
+    events_collection = _mongo_client.get_collection(mongo.mongo.Collection.EVENTS)
     event = events_collection.find_one({"event_number": event_id})
 
-    data_collection = mongo_client.get_collection(mongo.mongo.Collection.DATA)
+    data_collection = _mongo_client.get_collection(mongo.mongo.Collection.DATA)
     event_data = list(data_collection.find({"event_number": event_id}))
 
     for d in event_data:
-        get_data(mongo_client, d)
+        get_data(_mongo_client, d)
 
     return {"event": event,
             "event_data": event_data}
