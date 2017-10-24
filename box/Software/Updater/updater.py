e56e974b75c7acd0a2d875bda472a1739dbb68d0
Evan Hataishi <evanhata@hawaii.edu>
2017-10-24 10:27:39 -1000

    Improve updater

--- box/Software/Updater/updater.py
+++ box/Software/Updater/updater.py
@@ -1,47 +1,52 @@
 import urllib.request
 import subprocess
 from os import remove
+from os import path
+from os import makedirs
 import json_util
 import tarfile
 
-# TODO - Putting ./downloads in front of everything is kind of a bummer
-# TODO - Should I just download everything into current directory?
+download_dir_path = '/home/evan/Documents/test/'
+version_file = 'version.json'
+public_key = 'opq-signing-public.pem'
+update_package = 'opq.box.updates.tar.gz'
+signature = 'opq.box.updates.tar.gz.sig'
 
 def main():
-    version_file = 'version.json'
-    # True if already up to date
-    if (check_for_update(version_file)):
-        print('Already up to date')
-        quit()
+    check_download_path()
 
-    public_key = 'opq-signing-public.pem'
-    update_package = 'opq.box.updates.tar.gz'
-    signature = 'opq.box.updates.tar.gz.sig'
+    if (is_latest_version(version_file)):
+        print('Box already up to date')
+        quit()
 
     download_files(public_key, update_package, signature)
 
-    verified = verify_package(public_key, signature, update_package)
+    is_verified = verify_package(public_key, signature, update_package)
 
-    if verified:
+    if is_verified:
         open_package(update_package)
         run_update()
     else:
-        print('Unable to verify package')
+        print('Error: Unable to verify package')
 
-def check_for_update(version_file):
+def check_download_path():
+    if not path.exists(download_dir_path):
+        makedirs(download_dir_path)
+
+def is_latest_version(version_file):
     download_file_from_emilia(version_file)
-    new_version = json_util.file_to_dict('./downloads/' + version_file)
-    # NOTE Temporarily hardcoded local version file name
-    # TODO Returns empty dict if not found, should probably change this?
-    current_version = json_util.file_to_dict('./downloads/current_version.json')
-    return has_updated_version(current_version, new_version)
-
-def has_updated_version(current_version, new_version):
-    # add null safety
-    if current_version['version'] < new_version['version']:
+    server_version = json_util.file_to_dict(download_dir_path + version_file)
+    local_version = json_util.file_to_dict(download_dir_path + 'local_version.json')
+    # local version is invalid or doesn't exist -> run update
+    if not local_version or not 'version' in local_version:
         return False
-    else:
+    # Server version is invalid -> don't update
+    elif not server_version or not 'version' in server_version:
+        print('Server version has invalid format or does not exist')
         return True
+    elif local_version['version'] < server_version['version']:
+        return False
+    return True
 
 def download_files(public_key, update_package, signature):
     download_file_from_emilia(public_key)
@@ -49,41 +54,50 @@ def download_files(public_key, update_package, signature):
     download_file_from_emilia(signature)
 
 def download_file_from_emilia(file_name):
-    # TODO Currently downloading everything to /downloads
     try:
         url = 'http://emilia.ics.hawaii.edu:8151/' + file_name
-        urllib.request.urlretrieve(url, './downloads/' + file_name)
+        urllib.request.urlretrieve(url, download_dir_path + file_name)
     except Exception as e:
-        print("Error downloading \"" + file_name + "\": " + str(e))
+        print('Error downloading \'' + file_name + '\': ' + str(e))
         quit()
 
-# TODO catch errors/output for subprocess
 def verify_package(public_key, signature, update_package):
-    subprocess.call(['openssl', 'base64', '-d', \
-        '-in', './downloads/' + signature, \
-        '-out', './downloads/' + signature + '.bin'])
-    verified = subprocess.check_output(['openssl', 'dgst', \
-        '-sha256', '-verify', \
-        './downloads/' + public_key, '-signature', \
-        './downloads/' + signature + '.bin', \
-        './downloads/' + update_package])
-    remove('./downloads/' + signature + '.bin')
+    try:
+        subprocess.call(['openssl', 'base64', '-d', \
+            '-in', download_dir_path + signature, \
+            '-out', download_dir_path + signature + '.bin'])
+        verified = subprocess.check_output(['openssl', 'dgst', \
+            '-sha256', '-verify', \
+            download_dir_path + public_key, '-signature', \
+            download_dir_path + signature + '.bin', \
+            download_dir_path + update_package])
+        remove(download_dir_path + signature + '.bin')
 
-    if 'Verified OK' in str(verified):
-        return True
-    return False
+        # rename downloaded 'version.json' as 'local_version.json'
+        subprocess.call(['mv', download_dir_path + version_file, \
+            download_dir_path + 'local_version.json'])
+
+        if 'Verified OK' in str(verified):
+            return True
+        return False
+    except Exception as e:
+        print('Error verifying package: ' + str(e))
+        quit()
 
 def open_package(update_package):
     try:
-        with tarfile.open('./downloads/' + update_package) as tar:
-            tar.extractall("./downloads")
-            #tar.extractall()
-            #print("Success!")
+        with tarfile.open(download_dir_path + update_package) as tar:
+            tar.extractall(download_dir_path)
+            print('Extracted package')
     except Exception as e:
-        print(e)
+        print('Error opening package: ' + str(e))
+        quit()
 
 def run_update():
-    subprocess.call('./downloads/update.sh')
+    try:
+        subprocess.call(download_dir_path + 'update.sh')
+    except Exception as e:
+        print('Error running update: ' + str(e))
 
 if __name__ == '__main__':
     main()
