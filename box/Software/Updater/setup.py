110e3d9c470587e63e33b28d335b70a8749d0b5b
Evan Hataishi <evanhata@hawaii.edu>
2017-10-30 23:46:28 -1000

    Finish and test setup.py

--- box/Software/Updater/setup.py
+++ box/Software/Updater/setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 
 setup(name='updater',
       version='0.1',
-      description='Test updater install',
-      #url='http://github.com/storborg/funniest',
+      description='Install opq updater',
       author='Evan Hataishi',
       author_email='evanhata@hawaii.edu',
       license='MIT',
-      py_modules=['updater.py', 'json_util.py']
+      scripts={'updater.py': ['*']}
       )
