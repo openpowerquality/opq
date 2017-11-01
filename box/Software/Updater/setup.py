edb48734f3259b1150464bc8f358634c0f5b9a60
Evan Hataishi <evanhata@hawaii.edu>
2017-10-31 22:18:48 -1000

    Finish setup script

--- box/Software/Updater/setup.py
+++ box/Software/Updater/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='updater',
       version='0.1',
-      description='Install opq updater',
+      description='Install opq box updater',
       author='Evan Hataishi',
       author_email='evanhata@hawaii.edu',
       license='MIT',
-      scripts={'updater.py': ['*']},
+      scripts={'box_updater.py': ['*']},
       data_files=[('config.json'), ('requirements.txt')]
       )
