ba0b9aa76e969ea851b5e24b165e45df39ec4d2f
anthonyjchriste <achriste@hawaii.edu>
2017-11-15 10:53:37 -1000

    Add box constants to mauka for analysis which also incudes calibration constants

---
+++ mauka/box/__init__.py
@@ -0,0 +1,5 @@
+from mongo.mongo import get_box_calibration_constants
+
+SAMPLES_PER_CYCLE = 200
+SAMPLE_RATE_HZ = 12000
+CALIBRATION_CONSTANTS = get_box_calibration_constants()
--- mauka/mongo/mongo.py
+++ mauka/mongo/mongo.py
@@ -1,11 +1,22 @@
 import collections
+import typing
 
 import numpy
 import pymongo
 import gridfs
 
+
 def to_s16bit(data):
     return numpy.frombuffer(data, numpy.int16)
+
+
+def get_box_calibration_constants(mongo_client=None) -> typing.Dict[int, float]:
+    _mongo_client = mongo_client if mongo_client is not None else OpqMongoClient()
+    calibration_constants = _mongo_client.get_collection(Collection.CALIBRATION_CONSTANTS).find({}, {'_id', False})
+    r = {}
+    for device_constant in calibration_constants:
+        r[device_constant["device_id"]] = device_constant["calibration_constant"]
+    return r
 
 
 def get_data(mongo_client, event_data):
@@ -31,6 +42,7 @@ def load_event(event_id: int, mongo_client=None):
     return {"event": event,
             "event_data": event_data}
 
+
 class BoxEventType:
     """String enumerations and constants for event types"""
     FREQUENCY_DIP = "FREQUENCY_SAG"
@@ -46,10 +58,12 @@ class Collection:
     BOX_EVENTS = "boxEvents"
     DATA = "data"
     EVENTS = "events"
+    CALIBRATION_CONSTANTS = "CalibrationConstants"
 
 
 class OpqMongoClient:
     """Convenience mongo client for easily operating on OPQ data"""
+
     def __init__(self, host="127.0.0.1", port=27017, db="opq"):
         self.client = pymongo.MongoClient(host, port)
         self.db = self.client[db]
@@ -103,18 +117,25 @@ class OpqMongoClient:
         return self.fs.find_one({"filename": fid}).read()
 
     def export_data(self, out_dir: str, start_ts_ms_utc: int, end_ts_ms_utc: int):
-        measurements = self.measurements_collection.find({"$and": [{"timestamp_ms": {"$gte": start_ts_ms_utc}}, {"timestamp_ms": {"$lte": end_ts_ms_utc}}]}, {"_id": False})
+        measurements = self.measurements_collection.find(
+                {"$and": [{"timestamp_ms": {"$gte": start_ts_ms_utc}}, {"timestamp_ms": {"$lte": end_ts_ms_utc}}]},
+                {"_id": False})
         device_id_to_measurements = collections.defaultdict(list)
 
         for measurement in measurements:
             device_id_to_measurements[measurement["device_id"]].append(measurement)
 
         for device_id, measurements in device_id_to_measurements.items():
-            path = out_dir + "/measurements_" + str(device_id) + "_" + str(start_ts_ms_utc) + "_" + str(end_ts_ms_utc) + ".txt"
+            path = out_dir + "/measurements_" + str(device_id) + "_" + str(start_ts_ms_utc) + "_" + str(
+                end_ts_ms_utc) + ".txt"
             with open(path, "w") as out:
-                out.writelines(map(lambda m: str(m["timestamp_ms"]) + "," + str(m["frequency"]) + "," + str(m["voltage"]) + "\n", measurements))
+                out.writelines(
+                    map(lambda m: str(m["timestamp_ms"]) + "," + str(m["frequency"]) + "," + str(m["voltage"]) + "\n",
+                        measurements))
 
-        event_ids = map(lambda event: event["event_number"], self.events_collection.find({"$and": [{"event_start": {"$gte": start_ts_ms_utc}}, {"event_start": {"$lte": end_ts_ms_utc}}]}, ["event_number"]))
+        event_ids = map(lambda event: event["event_number"], self.events_collection.find(
+                {"$and": [{"event_start": {"$gte": start_ts_ms_utc}}, {"event_start": {"$lte": end_ts_ms_utc}}]},
+                ["event_number"]))
         events = map(lambda event_id: load_event(event_id, self), event_ids)
 
         for event in events:
@@ -123,12 +144,16 @@ class OpqMongoClient:
             event_number = str(event_meta["event_number"])
             path = out_dir + "/event_" + event_number + ".txt"
             with open(path, "w") as out:
-                out.write(str(event_meta["type"]) + "," + str(event_meta["description"]) + "," + str(event_meta["boxes_triggered"]) + "," +
-                          str(event_meta["boxes_received"]) + "," + str(event_meta["event_start"]) + "," + str(event_meta["event_end"]) + "\n")
+                out.write(str(event_meta["type"]) + "," + str(event_meta["description"]) + "," + str(
+                        event_meta["boxes_triggered"]) + "," +
+                          str(event_meta["boxes_received"]) + "," + str(event_meta["event_start"]) + "," + str(
+                        event_meta["event_end"]) + "\n")
                 # out.write(str(event_data) + "\n")
                 if len(event_data) > 0:
                     for ed in event_data:
-                        out.write(str(ed["box_id"]) + "," + str(len(ed["data"])) + "," + str(ed["event_start"]) + "," + str(ed["event_end"]) + "\n")
+                        out.write(
+                            str(ed["box_id"]) + "," + str(len(ed["data"])) + "," + str(ed["event_start"]) + "," + str(
+                                    ed["event_end"]) + "\n")
                         for datum in ed["data"]:
                             out.write(str(datum) + "\n")
 
@@ -136,8 +161,7 @@ class OpqMongoClient:
 if __name__ == "__main__":
     mongo_client = OpqMongoClient()
 
-    start_ts_ms_utc = 1508781600000 # Oct 23 8AM HST
-    end_ts_ms_utc   = 1508868000000 # Oct 24 8AM HST
+    start_ts_ms_utc = 1508781600000  # Oct 23 8AM HST
+    end_ts_ms_utc = 1508868000000  # Oct 24 8AM HST
 
     mongo_client.export_data("/Users/anthony/Development/opq/mauka/scrap", start_ts_ms_utc, end_ts_ms_utc)
-
