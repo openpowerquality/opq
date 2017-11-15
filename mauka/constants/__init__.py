83d1bd21a23136b2a8d319cb641d5c02ea37dd1e
anthonyjchriste <achriste@hawaii.edu>
2017-11-15 11:33:27 -1000

    Change box namespace to constants

--- mauka/constants/__init__.py
+++ mauka/constants/__init__.py
@@ -1,5 +1,5 @@
 from mongo.mongo import get_box_calibration_constants
 
-SAMPLES_PER_CYCLE = 200
-SAMPLE_RATE_HZ = 12000
+SAMPLES_PER_CYCLE = 200.0
+SAMPLE_RATE_HZ = 12000.0
 CALIBRATION_CONSTANTS = get_box_calibration_constants()
