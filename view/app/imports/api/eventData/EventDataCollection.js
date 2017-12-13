f64eb2f127c5355ee7c3520683f4f6ee811e4d95
David Aghalarpour <aghalarp@hawaii.edu>
2017-12-13 13:24:08 -1000

    Add itic and thd fields to collection schema

--- view/app/imports/api/eventData/EventDataCollection.js
+++ view/app/imports/api/eventData/EventDataCollection.js
@@ -19,9 +19,11 @@ class EventDataCollection extends BaseCollection {
           event_number: {type: Number},
           time_stamp: {type: [Number]}, // For research purposes; OPQView can ignore.
           //data: {type: [Number]} // These values need to be divided by device calibration constant.
-          data: {type: String} // Stores the GridFs filename. Format is 'event_eventNumber_boxId'
+          data: {type: String}, // Stores the GridFs filename. Format is 'event_eventNumber_boxId'
+          thd: {type: Number},
+          itic: {type: String}
         }),
-        Meteor.settings.opqRemoteMongoUrl
+        Meteor.settings.mongoUrl
     );
 
     this.publicationNames = {
