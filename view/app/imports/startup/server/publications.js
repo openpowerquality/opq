1c3af6c2bc807aef7ea6bfcf8e09bfb806482ca3
David Aghalarpour <aghalarp@hawaii.edu>
2018-01-24 13:55:40 -1000

    Update publications startup file

--- view/app/imports/startup/server/publications.js
+++ view/app/imports/startup/server/publications.js
@@ -1,6 +1,8 @@
 import { Measurements } from '../../api/measurements/MeasurementsCollection.js';
+import { Events } from '../../api/events/EventsCollection';
 import { BoxEvents } from '../../api/box-events/BoxEventsCollection.js';
 
 
 Measurements.publish();
+Events.publish();
 BoxEvents.publish();
