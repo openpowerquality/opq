0330619024a6ba3203b8b1b5587509a9233dbf5e
David Aghalarpour <aghalarp@hawaii.edu>
2018-01-26 12:20:23 -1000

    Client side user signup now properly encrypted using Accounts package

--- view/app/imports/ui/pages/signup/signup.js
+++ view/app/imports/ui/pages/signup/signup.js
@@ -4,29 +4,27 @@ import { Accounts } from 'meteor/accounts-base';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { createUser } from '../../../api/users/UsersCollectionMethods';
+import { setUserRole } from '../../../api/users/UsersCollectionMethods';
 import './signup.html';
 import '../../components/form-controls/text-form-control.html';
-// import { signupPageSchema } from '../../../utils/schemas.js';
-// import { createPerson } from '../../../api/person/PersonCollectionMethods.js';
 
 Template.signup.onCreated(function signupOnCreated() {
   const template = this;
 
-  // Define signup form schema so we can validate input. (This should be elsewhere).
+  // Define form error message. These should probably be defined elsewhere, as they are general use case messages.
   SimpleSchema.messages({
     passwordMismatch: 'Passwords do not match one another',
   });
 
+  // Define signup form schema so we can validate input.
   template.signupFormSchema = new SimpleSchema({
     firstName: { type: String },
     lastName: { type: String },
-    email: { // Accounts-password
+    email: {
       type: String,
-      label: 'E-mail *',
       regEx: SimpleSchema.RegEx.Email,
     },
-    password: { // Accounts-password
+    password: {
       type: String,
     },
     confirmPassword: {
@@ -40,7 +38,6 @@ Template.signup.onCreated(function signupOnCreated() {
   });
 
   template.validationContext = template.signupFormSchema.namedContext('Signup_Page');
-  // template.validationContext = signupPageSchema.namedContext('Signup_Page');
   template.formSubmissionErrors = new ReactiveVar();
 });
 
@@ -48,7 +45,6 @@ Template.signup.events({
   'submit .signup-form': function (event) {
     event.preventDefault();
     const template = Template.instance();
-    // console.log(template.validationContext);
 
     const firstName = event.target.firstName.value;
     const lastName = event.target.lastName.value;
@@ -57,53 +53,30 @@ Template.signup.events({
     const confirmPassword = event.target.confirmPassword.value;
 
     const formData = { firstName, lastName, email, password, confirmPassword };
-    // console.log(formData);
 
     // Clear old validation errors, clean data, and re-validate.
     template.validationContext.resetValidation();
     template.signupFormSchema.clean(formData);
-    // signupPageSchema.clean(formData);
     template.validationContext.validate(formData);
-    console.log('ValidationContext isValid? ', template.validationContext.isValid());
 
     // Continue upon validation success.
     if (template.validationContext.isValid()) {
-      createUser.call({ firstName, lastName, email, password }, (error, newUserID) => {
+      Accounts.createUser({
+        email: formData.email,
+        password: formData.password,
+        first_name: formData.firstName,
+        last_name: formData.lastName,
+      }, (error, result) => { // eslint-disable-line no-unused-vars
         if (error) {
-          console.log(error); // eslint-disable-line no-console
           template.formSubmissionErrors.set(error);
         } else {
-          console.log(`User created successfully: ${newUserID}`); // eslint-disable-line no-console
+          // User created successfully and is automatically logged in at this point.
+          const loggedInUserID = Meteor.userId();
+          // Now set user role.
+          setUserRole.call({ userID: loggedInUserID, role: 'user' });
           FlowRouter.go('/'); // Redirect to home page.
         }
       });
-      // Create User, then Person in subsequent callback.
-      // Accounts.createUser({ email: email, password: password }, function (error) {
-      //   if (error) {
-      //     console.log(error); // eslint-disable-line no-console
-      //     template.formSubmissionErrors.set(error.reason);
-      //     // template.validationContext.addInvalidKeys()
-      //   } else {
-      //     // User created successfully and is automatically logged in.
-      //     const currUserId = Meteor.userId();
-      //
-      //     // Create Person obj with userId reference, then call createPerson method.
-      //     const person = {
-      //       userId: currUserId,
-      //       firstName: firstName,
-      //       lastName: lastName,
-      //     };
-      //
-      //     createPerson.call(person, (error, personId) => { // eslint-disable-line no-shadow
-      //       if (error) {
-      //         console.log(error); // eslint-disable-line no-console
-      //       } else {
-      //         console.log(`Inserted Person successfully: ${personId}`); // eslint-disable-line no-console
-      //         FlowRouter.go('/'); // Redirect to home page.
-      //       }
-      //     });
-      //   }
-      // });
     }
   },
 });
