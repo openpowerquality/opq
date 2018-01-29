5d6613c98e82d350c8011aded1e2abe30165c1b3
David Aghalarpour <aghalarp@hawaii.edu>
2018-01-29 06:01:53 -1000

    Refactor BoxEvents collection methods

--- view/app/imports/api/box-events/BoxEventsCollectionMethods.js
+++ view/app/imports/api/box-events/BoxEventsCollectionMethods.js
@@ -3,29 +3,11 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { BoxEvents } from './BoxEventsCollection.js';
 
-export const getRecentEventDataReqIds = new ValidatedMethod({
-  name: 'BoxEvents.getRecentEventDataReqIds',
-  validate: new SimpleSchema({
-    numEvents: { type: Number },
-  }).validator({ clean: true }),
-  run({ numEvents }) {
-    if (!this.isSimulation) {
-      const numberOfEvents = (numEvents > 100) ? 100 : numEvents;
-
-      // Find events that actually have waveform data, which we can determine by checking whether the
-      // eventData object has more than the default two fields (_id and request_id).
-      // For the future, event data should have an array field that lists all deviceIds that the event included.
-      const eventReqIds = BoxEvents.find({}, { sort: { _id: -1 } })
-          .fetch()
-          .filter(event => Object.keys(event).length > 2)
-          .map(event => ({
-            _id: event._id,
-            request_id: event.request_id,
-          }));
-
-      return eventReqIds.slice(0, numberOfEvents);
-    }
-    return null;
+export const totalBoxEventsCount = new ValidatedMethod({
+  name: 'BoxEvents.totalBoxEventsCount',
+  validate: new SimpleSchema().validator({ clean: true }),
+  run() {
+    return BoxEvents.find({}).count();
   },
 });
 
