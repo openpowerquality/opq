4f3d6fc942a5b79a8ac57ac0cc3fbb9f524fbb5a
David Aghalarpour <aghalarp@hawaii.edu>
2018-01-29 06:03:02 -1000

    Add additional OpqBoxes collection methods

--- view/app/imports/api/opq-boxes/OpqBoxesCollectionMethods.js
+++ view/app/imports/api/opq-boxes/OpqBoxesCollectionMethods.js
@@ -2,6 +2,14 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { OpqBoxes } from './OpqBoxesCollection';
 
+export const totalOpqBoxesCount = new ValidatedMethod({
+  name: 'OpqBoxes.totalOpqBoxesCount',
+  validate: new SimpleSchema().validator({ clean: true }),
+  run() {
+    return OpqBoxes.find({}).count();
+  },
+});
+
 export const getBoxCalibrationConstant = new ValidatedMethod({
   name: 'OpqBoxes.getBoxCalibrationConstant',
   validate: new SimpleSchema({
@@ -11,6 +19,19 @@ export const getBoxCalibrationConstant = new ValidatedMethod({
     if (!this.isSimulation) {
       const opqBox = OpqBoxes.findOne({ box_id });
       return (opqBox) ? opqBox.calibration_constant : null;
+    }
+    return null;
+  },
+});
+
+export const getBoxIDs = new ValidatedMethod({
+  name: 'OpqBoxes.getBoxIDs',
+  validate: new SimpleSchema({}).validator({ clean: true }),
+  run() {
+    if (!this.isSimulation) {
+      const opqBoxes = OpqBoxes.find({});
+      const boxIDs = opqBoxes.map(box => box.box_id);
+      return boxIDs;
     }
     return null;
   },
