f84259aed504da20a88377e467c45644555cf9e9
anthonyjchriste <achriste@hawaii.edu>
2018-03-19 13:23:48 -1000

    Start documenting sim module and provide definitions for example frequency filters.

--- util/box/sim/sim.py
+++ util/box/sim/sim.py
@@ -64,10 +64,22 @@ class OpqDeviceFifo:
 
 
 def as_vrms(v: int, calibration_constant: float = 152.0) -> float:
+    """
+    Converts a signed 16-bit simulated DAC reading into Vrms.
+    :param v: The 16-bit signed integer simulated DAC reading.
+    :param calibration_constant: Calibration constant for device we are converted for.
+    :return: Vrms
+    """
     return (v / calibration_constant) / SQRT_2
 
 
 def scale_to_sint16(v: float, calibration_constant: float = 152.0) -> int:
+    """
+    Converts a raw voltage reading to a signed 16-bit integer.
+    :param v: Raw voltage.
+    :param calibration_constant: Device calibration constant.
+    :return: Scaled and signed 16-bit integer.
+    """
     r = round(v * calibration_constant)
     if r < S16_MIN:
         return S16_MIN
@@ -77,61 +89,132 @@ def scale_to_sint16(v: float, calibration_constant: float = 152.0) -> int:
 
 
 class DelayedStateFilter:
+    """
+    This class represents a base class for filters who change the state of waveform generation using a delay where the
+    delay is specified in number of samples.
+    """
     def __init__(self,
                  initial_value: float,
                  target_value: float,
                  delay_samples: int,
                  state_arg: str):
+        """
+        Initializes this delayed state filter
+        :param initial_value: The initial value of the filter
+        :param target_value: The target value of the filter
+        :param delay_samples: The number of samples to get from initial_value to target_value.
+        :param state_arg: The state field that is being modified.
+        """
 
-        self.initial_value = initial_value
-        self.delay_samples = delay_samples
-        self.i = 0
-        self.d = (target_value - initial_value) / delay_samples
-        self.state_arg = state_arg
+        self.initial_value: float = initial_value
+        """The starting state value"""
+
+        self.delay_samples: int = delay_samples
+        """The total number of samples to delay before hitting target value"""
+
+        self.i: int = 0
+        """Index into the filter state"""
+
+        self.d: float = (target_value - initial_value) / delay_samples
+        """Delta that selected state arg changes for each sample"""
+
+        self.state_arg: str = state_arg
+        """Name of the value in the waveform state that will be changing (e.g. amplitude, frequency, etc..."""
 
     def has_next_state(self) -> bool:
+        """
+        :return: If this filter has a next state or not.
+        """
         return self.i < self.delay_samples
 
     def next_state(self) -> typing.Dict[str, typing.Dict[str, float]]:
+        """
+        :return: The next state of this filter
+        """
         s = {"state": {self.state_arg: self.initial_value + (self.i * self.d)}}
         self.i += 1
         return s
 
 
 class NopFilter(DelayedStateFilter):
+    """
+    The nop filter does nothing for the specified number of samples.
+    The state of the waveform generator will remain as it was since the last state change.
+    """
     def __init__(self, delay_samples: int):
+        """
+        :param delay_samples: Number of samples for this filter to do nothing for.
+        """
         super().__init__(0, 0, delay_samples, "")
 
     def next_state(self) -> typing.Dict:
+        """
+        :return: An empty state.
+        """
         super().next_state()
         return {}
 
 
 class DelayedAmplitudeFilter(DelayedStateFilter):
+    """
+    This filter changes the amplitude from it's current amplitude to a new amplitude over a given number of cycles.
+    """
     def __init__(self, initial_amplitude: float, target_amplitude: float, delay_samples: int):
+        """
+        :param initial_amplitude: Initial waveform amplitude (raw voltage not RMS)
+        :param target_amplitude: Target waveform amplitude
+        :param delay_samples: Number of samples it takes to go from initial to target
+        """
         super().__init__(initial_amplitude, target_amplitude, delay_samples, "amplitude")
 
 
 class DelayedFrequencyFilter(DelayedStateFilter):
+    """
+    This filter changes the frequency from it's current frequency to a new frequency over a given number of cycles.
+    """
     def __init__(self, initial_frequency: float, target_frequency: float, delay_samples: int):
         super().__init__(initial_frequency, target_frequency, delay_samples, "frequency")
 
 
 class FilterManager:
+    """
+    This class manages the state of the current list of filters.
+    """
     def __init__(self,
                  initial_amplitude: float,
                  initial_frequency: float,
                  filter_definitions: typing.List[typing.Dict],
                  does_repeat: bool):
-        self.initial_amplitude = initial_amplitude
-        self.initial_frequency = initial_frequency
+        """
+        :param initial_amplitude: Amplitude when this filter was created
+        :param initial_frequency: Frequency when this filter was created
+        :param filter_definitions: Filters currently being managed
+        :param does_repeat: Does the list of filters repeat after finishing with the last filter?
+        """
+
+        self.initial_amplitude: float = initial_amplitude
+        """Amplitude when this filter was created"""
+
+        self.initial_frequency: float = initial_frequency
+        """Frequency when this filter was created"""
+
         self.filters = list(map(self.make_filter, filter_definitions))
-        self.does_repeat = does_repeat
-        self.i = 0
+        """Filters currently being manage"""
+
+        self.does_repeat: bool = does_repeat
+        """Does the list of filters repeat after finishing with the last filter?"""
+
+        self.i: int = 0
+        """Index into the list of filters"""
 
     def make_filter(self, filter_definition: typing.Dict) -> typing.Union[NopFilter,
                                                                           DelayedAmplitudeFilter,
                                                                           DelayedFrequencyFilter]:
+        """
+        Given a filter definition, parse the required fields and create an instance of the request filter.
+        :param filter_definition: Definition provides as JSON string.
+        :return: Filter instance from definition.
+        """
         name = filter_definition["name"]
 
         if name == "vrms":
@@ -147,10 +230,19 @@ class FilterManager:
         else:
             raise RuntimeError("Unknown filter name {}".format(name))
 
-    def current_filter(self) -> typing.Union[NopFilter, DelayedAmplitudeFilter]:
+    def current_filter(self) -> typing.Union[NopFilter,
+                                             DelayedAmplitudeFilter,
+                                             DelayedFrequencyFilter]:
+        """
+        :return: The current filter.
+        """
         return self.filters[self.i]
 
-    def next_filter(self) -> typing.Union[NopFilter, DelayedAmplitudeFilter]:
+    def next_filter(self) -> typing.Union[NopFilter,
+                                          DelayedAmplitudeFilter,
+                                          DelayedFrequencyFilter]:
+        """Returns the next filter or None if there are no more filters"""
+
         self.i += 1
         # Not at end of filter list
         if self.i < len(self.filters):
@@ -168,6 +260,7 @@ class FilterManager:
                 return None
 
     def next_state(self) -> typing.Dict:
+        """Returns the next state from the currently filter."""
         current_filter = self.current_filter()
         if current_filter.has_next_state():
             return current_filter.next_state()
@@ -180,17 +273,41 @@ class FilterManager:
 
 
 class WaveformGenerator:
+    """
+    This class generates a simulated waveform where the amplitude, frequency, phase, and sample rate can be altered
+    to change the state of the waveform generator.
+    """
     def __init__(self, debug: bool = False):
         self.amplitude: float = 120.0 * SQRT_2
+        """Amplitude of the waveform"""
+
         self.frequency: float = 60.0
+        """Frequency of the waveform"""
+
         self.phase: float = 0.0
+        """Phase of the waveform"""
+
         self.sample_rate_hz: float = 12000.0
+        """Sampling rate of the waveform"""
+
         self.samples_per_cycle: int = 200
+        """Number of samples per cycle in the waveform"""
+
         self.generator: typing.Generator[typing.Tuple[int, float]] = None
+        """A Python generator that lazily produces the waveform"""
+
         self.filter_manager: FilterManager = None
+        """Allows us to apply filters to the waveform"""
+
         self.debug: bool = debug
+        """Should we print information to stdout in debug format?"""
 
     def safe_update(self, state: typing.Dict):
+        """
+        Udates the state of the waveform generator.
+        :param state:
+        :return:
+        """
         if len(state) == 0:
             return
 
