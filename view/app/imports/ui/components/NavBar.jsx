b05a0ec9d634f9dc90fe6db16c36832308742a5a
Philip Johnson <johnson@hawaii.edu>
2018-03-19 16:40:28 -1000

    Make the navbar nicer.

--- view/app/imports/ui/components/NavBar.jsx
+++ view/app/imports/ui/components/NavBar.jsx
@@ -7,14 +7,18 @@ import { Menu, Image } from 'semantic-ui-react';
 
 class NavBar extends React.Component {
   render() {
+    const divStyle = { color: '#2185D0', paddingLeft: '2px', fontWeight: 'bold' };
     return (
       <Menu stackable borderless>
-        <Menu.Item as={NavLink} exact to="/"><Image width="20px" src="/images/opqlogo.png"/>&nbsp;OPQView</Menu.Item>
-        <Menu.Item position="right" as={NavLink} exact to="/aboutus">About Us</Menu.Item>
+        <Menu.Item as={NavLink} exact to="/">
+          <Image width="20px" src="/images/opqlogo.png"/>
+          <div style={divStyle}>OPQView</div>
+        </Menu.Item>
+        <Menu.Item position="right" as={NavLink} exact to="/aboutus"><div style={divStyle}>About Us</div></Menu.Item>
         {this.props.currentUser === '' ? (
-          <Menu.Item as={NavLink} exact to="/signin">Login</Menu.Item>
+          <Menu.Item as={NavLink} exact to="/signin"><div style={divStyle}>Login</div></Menu.Item>
         ) : (
-          <Menu.Item as={NavLink} exact to="/signout">Logout</Menu.Item>
+          <Menu.Item as={NavLink} exact to="/signout"><div style={divStyle}>Logout</div></Menu.Item>
         )}
       </Menu>
     );
