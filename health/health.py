a0c1a0c1fb8a746cf6494d215a114d61a0989469
Evan Hataishi <evan@emilia.ics.hawaii.edu>
2018-03-21 13:27:17 -1000

    implement view check and logging

--- health/health.py
+++ health/health.py
@@ -1,14 +1,48 @@
-import zmq
+import json
+import argparse
+from requests import get
+from time import ctime
+from time import sleep
 
-def main():
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.connect("tcp://127.0.0.1:9881")
-    socket.setsockopt(zmq.SUBSCRIBE, '')
+def write_file(file_name, message):
+    try:
+        with open(file_name, 'a') as log_file:
+            log_file.write(message + '\n')
+    except:
+        print('Unable to write to ' + file_name)
+        exit()
 
+def file_to_dict(file_name):
+    try:
+        with open(file_name) as opened_file:
+            json_data = json.load(opened_file)
+            return json_data
+    except:
+        print('Unable to open ' + file_name)
+        exit()
+
+def run_view(config, log_file):
+    sleep_time = config['interval']
+    url = config['url']
     while True:
-        measurement = socket.recv_string()
-        print(message)
+        status = get(url).status_code
+        if status != 200:
+            message = ctime() + ' View Health Error: Status Code ' + str(status)
+            write_file(log_file, message)
+        else:
+            message = ctime() + ' View Healthy: Status Code ' + str(status)
+            write_file(log_file, message)
+        sleep(sleep_time)
+
+def main(config_file, log_file):
+    config = file_to_dict(config_file)
+    view_config = config[4]
+    run_view(view_config, log_file)
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='test')
+    parser.add_argument('-config', nargs=1, help='Name of config file', default=['config.json'])
+    parser.add_argument('-log', nargs=1, help='Name of log file', default=['log.txt'])
+    args = parser.parse_args()
+    main(args.config[0], args.log[0])
