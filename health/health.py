60e1900cfb53a37c7981b5a3b4e2befadc8a4eb0
Evan Hataishi <evan@emilia.ics.hawaii.edu>
2018-03-26 18:10:26 -1000

    Read all box messages

--- health/health.py
+++ health/health.py
@@ -1,5 +1,7 @@
 import json
 import argparse
+import zmq
+import protobuf.opq_pb2
 from requests import get
 from time import ctime
 from time import sleep
@@ -22,7 +24,7 @@ def file_to_dict(file_name):
         print('Unable to open ' + file_name)
         exit()
 
-def run_view(config, log_file):
+def check_view(config, log_file):
     sleep_time = config['interval']
     url = config['url']
     while True:
@@ -35,14 +37,34 @@ def run_view(config, log_file):
             write_file(log_file, message)
         sleep(sleep_time)
 
+def check_boxes(config, port, log_file):
+    sleep_time = config['interval']
+    
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.setsockopt(zmq.SUBSCRIBE, b"")
+    socket.connect(port)
+
+    while True:
+        measurement = socket.recv_multipart()
+        trigger_message = protobuf.opq_pb2.TriggerMessage()
+        trigger_message.ParseFromString(measurement[1])
+        print(trigger_message)
+
 def main(config_file, log_file):
     health_config = file_to_dict(config_file)
 
-    view_config = health_config[4]
-    view_thread = Thread(target=run_view, args=(view_config,log_file, ))
-    view_thread.start()
+    #view_config = health_config[4]
+    #view_thread = Thread(target=check_view, args=(view_config,log_file, ))
+    #view_thread.start()
 
-    view_thread.join()
+    box_config = health_config[1]
+    zmq_port = health_config[0]['port']
+    box_thread = Thread(target=check_boxes, args=(box_config,zmq_port,log_file))
+    box_thread.start()
+    
+    #view_thread.join()
+    box_thread.join()
 
 def parse_cmd_args():
     parser = argparse.ArgumentParser(description='Get config and log file names')
