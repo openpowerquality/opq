da4414acabdb55ae559073bf5c9e8d15f354d1fe
Anthony Christe <anthony.christe@gmail.com>
2018-03-29 10:31:53 -1000

    Update deploy scripts for mauka

--- deploy/mauka/deploy.sh
+++ deploy/mauka/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Deploying Mauka..."
-
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -9,6 +7,22 @@ fi
 
 set -o xtrace
 
-# Deploy stuff goes here.
+cp mauka-service.sh /etc/init.d/mauka
+update-rc.d mauka defaults
+
+cp mauka-cli.sh /usr/local/bin/mauka-cli
+chmod +x /usr/local/bin/mauka-cli
+
+cp mauka-log.sh /usr/local/bin/mauka-log
+chmod +x /usr/local/bin/mauka-log
+
+mkdir -p /usr/local/bin/opq
+chown -R opq:opq /usr/local/bin/opq
+cd ../..
+cp -R mauka /usr/local/bin/opq/.
+cp mauka/config.json /etc/opq/mauka/config.json
+
+mkdir -p /var/log/opq
+chown -R opq:opq /var/log/opq
 
 set +o xtrace
