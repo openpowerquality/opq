c20f7c1b9ea843872cda346906714c95d4439433
Philip Johnson <johnson@hawaii.edu>
2018-04-09 16:16:56 -1000

    Set up deploy dir for health.

---
+++ health/deploy/deploy-transfer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+timestamp=$(date +%Y%m%d_%H%M%S)
+mkdir $timestamp
+cp ../health.py $timestamp
+cp ../config.json $timestamp
+# figure out what to put into deploy file for protobuf.
+cd ..
+tar czf $timestamp.tar.gz $timestamp
+rm -rf $timestamp
+scp -P 29862 $timestamp.tar.gz opquser@emilia.ics.hawaii.edu:view
+
+set +x
