8f2831fcde759c6d0ad645347a95a66ecc87700a
Anthony Christe <anthony.christe@gmail.com>
2018-04-11 07:31:26 -1000

    Start mongod with replica sets on higher port numbers until we transition.

--- util/mongod/install/start-mongod.sh
+++ util/mongod/install/start-mongod.sh
@@ -5,6 +5,6 @@ LOG_BASE=/var/log/mongodb
 MONGOD=/usr/local/bin/mongodb/bin/mongod
 
 # Start replica sets
-${MONGOD} --replSet opq-replica-set --port 27017 --dbpath ${DB_BASE}/rs0 --fork --logpath ${LOG_BASE}/rs0.loq
-${MONGOD} --replSet opq-replica-set --port 27018 --dbpath ${DB_BASE}/rs1 --fork --logpath ${LOG_BASE}/rs1.loq
-${MONGOD} --replSet opq-replica-set --port 27019 --dbpath ${DB_BASE}/rs2 --fork --logpath ${LOG_BASE}/rs2.loq
+${MONGOD} --replSet opq-replica-set --port 27018 --dbpath ${DB_BASE}/rs0 --fork --logpath ${LOG_BASE}/rs0.loq
+${MONGOD} --replSet opq-replica-set --port 27019 --dbpath ${DB_BASE}/rs1 --fork --logpath ${LOG_BASE}/rs1.loq
+${MONGOD} --replSet opq-replica-set --port 27020 --dbpath ${DB_BASE}/rs2 --fork --logpath ${LOG_BASE}/rs2.loq
