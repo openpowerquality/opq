b1b2d839d08328a799b6e1e017b609f8bc2ded7e
David Aghalarpour <aghalarp@hawaii.edu>
2018-04-11 12:10:02 -0400

    Create BoxMapPage

---
+++ view/app/imports/ui/pages/BoxMapPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import BoxMap from '../components/BoxMap';
+
+const BoxMapPage = () => (
+    <Grid container stackable>
+      <Grid.Column width={16}>
+        <BoxMap/>
+      </Grid.Column>
+    </Grid>
+);
+
+export default BoxMapPage;
