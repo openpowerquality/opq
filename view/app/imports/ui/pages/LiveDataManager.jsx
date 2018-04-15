db28cab62e1dd6248a5b8d47aabc8fa00dd3aaa5
Kea Uehara <keal@hawaii.edu>
2018-04-15 12:01:58 -1000

    made page

---
+++ view/app/imports/ui/pages/LiveDataManager.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Loader } from 'semantic-ui-react';
+
+import { BoxOwners } from '../../api/users/BoxOwnersCollection';
+
+class LiveDataManager extends React.Component {
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  /** Actually renders the page. */
+  renderPage() {
+    return this.props.boxIDs;
+  }
+}
+
+/** Require an array of Stuff documents in the props. */
+LiveDataManager.propTypes = {
+  ready: PropTypes.bool.isRequired,
+  boxIDs: PropTypes.array,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  const sub = Meteor.subscribe('BoxOwners');
+  return {
+    ready: sub.ready(),
+    boxIDs: BoxOwners.findBoxIdsWithOwner(Meteor.username),
+  };
+})(LiveDataManager);
