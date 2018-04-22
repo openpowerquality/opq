28a82451f697f34d1162ee2a8393706e8e7bb452
Philip Johnson <johnson@hawaii.edu>
2018-04-22 09:07:35 -1000

    checkpoint

--- view/app/imports/ui/components/SystemHealth.jsx
+++ view/app/imports/ui/components/SystemHealth.jsx
@@ -5,6 +5,7 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import WidgetPanel from '../layouts/WidgetPanel';
 import { SystemStats } from '../../api/system-stats/SystemStatsCollection';
+import { Healths } from '../../api/health/HealthsCollection';
 
 /** Display system statistics. */
 class SystemHealth extends React.Component {
@@ -38,6 +39,7 @@ class SystemHealth extends React.Component {
   }
 
   renderPage() {
+    console.log(this.props.healths);
     const divStyle = { paddingLeft: '10px', paddingRight: '10px' };
     const headerStyle =
       { paddingLeft: '10px', paddingRight: '10px', textAlign: 'center', fontWeight: 'bold', marginBottom: '5px' };
@@ -61,14 +63,18 @@ class SystemHealth extends React.Component {
 /** Require an array of Stuff documents in the props. */
 SystemHealth.propTypes = {
   stats: PropTypes.object,
+  healths: PropTypes.array,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   const subscription = Meteor.subscribe(SystemStats.getPublicationName());
+  const subscription2 = Meteor.subscribe(Healths.getPublicationName());
   return {
     stats: SystemStats.findOne({}),
-    ready: subscription.ready(),
+    healths:
+      Healths.find({ timestamp: { $gt: new Date(Date.now() - (1000 * 62)) } }, { sort: { timestamp: -1 } }).fetch(),
+    ready: subscription.ready() && subscription2.ready(),
   };
 })(SystemHealth);
