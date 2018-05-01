ee6eaa0d8c9f81480f76ee8ec58c820a05036df6
Anthony Christe <anthony.christe@gmail.com>
2018-04-30 15:16:30 -1000

    Inject Makai pub/sub event messages into Mauka.

--- mauka/OpqMauka.py
+++ mauka/OpqMauka.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
 
     broker_process = plugins.start_mauka_pub_sub_broker(config)
     makai_bridge_process = plugins.start_makai_bridge(config)
+    makai_bridge_event_process = plugins.start_makai_event_bridge(config)
 
     # start-stop-daemon sends a SIGTERM, we need to handle it to gracefully shutdown mauka
     def sigterm_handler(signum, frame):
