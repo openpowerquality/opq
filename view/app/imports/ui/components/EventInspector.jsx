88664acbfc5904f2632f2a1f2654bb349a40439d
Kea Uehara <keal@hawaii.edu>
2018-05-08 14:51:13 -1000

    Working on displaying waveform

--- view/app/imports/ui/components/EventInspector.jsx
+++ view/app/imports/ui/components/EventInspector.jsx
@@ -14,13 +14,12 @@ class EventInspector extends React.Component {
   constructor(props) {
     super(props);
 
-    const end = new Date();
-    const start = new Date(end);
-    start.setDate(end.getDate() - 1);
+    const end = Moment();
+    const start = Moment(end).subtract(1, 'day');
 
     this.state = {
-      start,
-      end,
+      start: start.format('YYYY-MM-DDTHH:mm'),
+      end: end.format('YYYY-MM-DDTHH:mm'),
       loading: false,
       loaded: false,
       selectedBoxes: ['1'],
@@ -33,40 +32,33 @@ class EventInspector extends React.Component {
   <p>Event Inspector lets you search for an event and look at the details of it, such as the waveform at the time of 
   the event.</p>
   
-  <p>Start and End: Select a starting and ending date to search between.</p>
+  <p>Start and End: Select a starting and ending date/time to search between.</p>
   
   <p>Boxes: select one or more boxes whose events you are interested in.</p>
   `;
 
   render() {
     return (
-      <WidgetPanel title='Event Inspector'>
+      <WidgetPanel title='Event Inspector' helpText={this.helpText}>
         <Grid container><Grid.Column width={16}><Grid stackable>
           <Grid.Row>
-            <Grid.Column width={5}>
-              <Popup on='focus'
-                     trigger={<Input fluid placeholder='Input a starting date' label='Start'
-                                     value={Moment(this.state.start).format('MM/DD/YYYY HH:mm:ss')}/>}
-                     content={<Calendar onChange={this.changeStart} value={this.state.start}/>}/>
-              <Input value={this.state.test} error onChange={(event, data) => {
-                console.log(this.state.test);
-                this.setState({ test: data.value }); }}/>
-            </Grid.Column>
-            <Grid.Column width={5}>
-              <Popup on='focus'
-                     trigger={<Input fluid placeholder='Input an ending date' label='End'
-                                     value={Moment(this.state.end).format('MM/DD/YYYY HH:mm:ss')}/>}
-                     content={<Calendar onChange={this.changeEnd} value={this.state.end}/>}/>
+            <Grid.Column width={4}>
+              <Input fluid label='Start' type='datetime-local'
+                     defaultValue={this.state.start} onChange={this.changeStart}/>
             </Grid.Column>
             <Grid.Column width={4}>
+              <Input fluid label='End' type='datetime-local'
+                     defaultValue={this.state.end} onChange={this.changeEnd}/>
+            </Grid.Column>
+            <Grid.Column width={5}>
               <Dropdown multiple search selection fluid
                         placeholder='Boxes'
                         options={this.props.boxIDs.map(boxID => ({ text: `Box ${boxID}`, value: boxID }))}
                         onChange={this.changeSelectedBoxes}
                         defaultValue={this.state.selectedBoxes}/>
             </Grid.Column>
-            <Grid.Column width={2}>
-              <Button content='Search' fluid onClick={this.getEvents}/>
+            <Grid.Column width={3}>
+              <Button content='Submit' fluid onClick={this.getEvents}/>
             </Grid.Column>
           </Grid.Row>
           {this.state.loading ? (
@@ -101,6 +93,13 @@ class EventInspector extends React.Component {
                           {Moment(event.target_event_end_timestamp_ms).format('MM/DD/YYYY HH:mm:ss')}
                         </Table.Cell>
                       </Table.Row>
+                      <Table.Row>
+                        <Table.Cell colspan={5}>
+                          <strong>Waveform available for: </strong>
+                          {event.boxes_received.map(boxID => (
+                            <Button content={`Box ${boxID}`} onClick= />))}
+                        </Table.Cell>
+                      </Table.Row>
                     </Table.Body>
                   </Table>
                 ))}
@@ -112,14 +111,18 @@ class EventInspector extends React.Component {
     );
   }
 
-  changeStart = start => { this.setState({ start }); };
-  changeEnd = end => { this.setState({ end }); };
+  changeStart = (event, data) => { this.setState({ start: data.value }); };
+  changeEnd = (event, data) => { this.setState({ end: data.value }); };
   changeSelectedBoxes = (event, data) => { this.setState({ selectedBoxes: data.value }); };
 
   getEvents = () => {
     const { selectedBoxes, start, end } = this.state;
     this.setState({ loading: true }, () => {
-      getEventsInRange.call({ boxIDs: selectedBoxes, startTime_ms: start.getTime(), endTime_ms: end.getTime() },
+      getEventsInRange.call({
+          boxIDs: selectedBoxes,
+          startTime_ms: Moment(start).valueOf(),
+          endTime_ms: Moment(end).valueOf()
+        },
         (error, events) => {
           this.setState({ events, loading: false, loaded: true });
         });
