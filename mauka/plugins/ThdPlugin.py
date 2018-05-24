a4ff850dae41da66daef16971521d756d4607024
anthonyjchriste <achriste@hawaii.edu>
2018-05-23 17:49:37 -1000

    Take the optimizations as far as they can go.

--- mauka/plugins/ThdPlugin.py
+++ mauka/plugins/ThdPlugin.py
@@ -38,6 +38,21 @@ def closest_idx(array: numpy.ndarray, val: float) -> int:
     """
     return numpy.argmin(numpy.abs(array - val))
 
+def get_x(size):
+    return numpy.fft.fftfreq(size, 1 / constants.SAMPLE_RATE_HZ)
+
+def memoize(fn):
+    state = {}
+
+    def memo(v):
+        if v not in state:
+            state[v] = fn(v)
+        return state[v]
+
+    return memo
+
+
+memoized_get_x = memoize(get_x)
 
 def thd(waveform: numpy.ndarray) -> float:
     """
@@ -45,27 +60,48 @@ def thd(waveform: numpy.ndarray) -> float:
     :param waveform:
     :return:
     """
-    y = scipy.fftpack.fft(waveform)
-    x = numpy.fft.fftfreq(y.size, 1 / constants.SAMPLE_RATE_HZ)
+    y = numpy.abs(scipy.fftpack.fft(waveform))
+    # x = numpy.fft.fftfreq(y.size, 1 / constants.SAMPLE_RATE_HZ)
+    # x = memoized_get_x(y.size)
+    # print(x)
+    #
+    # new_x = []
+    # new_y = []
+    # for i in range(len(x)):
+    #     # print(x[i])
+    #     if x[i] >= 0:
+    #         new_x.append(x[i])
+    #         new_y.append(y[i])
+    #
+    # new_x = numpy.array(new_x)
+    # new_y = numpy.abs(y)
+    # print(y.size, new_y.size)
+    # print(closest_idx(x, 60.0))
+    # print(closest_idx(x, 120.0))
+    # print(closest_idx(x, 180.0))
+    # print(closest_idx(x, 240.0))
+    # print(closest_idx(x, 300.0))
+    # print(closest_idx(x, 360.0))
+    # print(closest_idx(x, 420.0))
 
-    new_x = []
-    new_y = []
-    for i in range(len(x)):
-        if x[i] >= 0:
-            new_x.append(x[i])
-            new_y.append(y[i])
-
-    new_x = numpy.array(new_x)
-    new_y = numpy.abs(numpy.array(new_y))
+    # nth_harmonic = {
+    #     1: new_y[closest_idx(x, 60.0)],
+    #     2: new_y[closest_idx(x, 120.0)],
+    #     3: new_y[closest_idx(x, 180.0)],
+    #     4: new_y[closest_idx(x, 240.0)],
+    #     5: new_y[closest_idx(x, 300.0)],
+    #     6: new_y[closest_idx(x, 360.0)],
+    #     7: new_y[closest_idx(x, 420.0)]
+    # }
 
     nth_harmonic = {
-        1: new_y[closest_idx(new_x, 60.0)],
-        2: new_y[closest_idx(new_x, 120.0)],
-        3: new_y[closest_idx(new_x, 180.0)],
-        4: new_y[closest_idx(new_x, 240.0)],
-        5: new_y[closest_idx(new_x, 300.0)],
-        6: new_y[closest_idx(new_x, 360.0)],
-        7: new_y[closest_idx(new_x, 420.0)]
+        1: y[12],
+        2: y[24],
+        3: y[36],
+        4: y[48],
+        5: y[60],
+        6: y[72],
+        7: y[84]
     }
 
     top = sq(nth_harmonic[2]) + sq(nth_harmonic[3]) + sq(nth_harmonic[4]) + sq(nth_harmonic[5])
@@ -108,16 +144,16 @@ class ThdPlugin(plugins.base.MaukaPlugin):
                 # We only care if this is the end of an anomaly
                 if prev_beyond_threshold:
                     self.debug("Found anomaly.")
-                    anomaly = mongo.make_anomaly_document(event_id,                                 # Event id
-                                                          box_id,                                   # box id
-                                                          mongo.BoxEventType.THD.value,              # Event name
-                                                          "unknown",                                # Location
-                                                          0,                                        # Start ts ms
-                                                          0,                                        # End ts ms
-                                                          0,                                        # Duration ms
-                                                          prev_idx,                                 # Start idx
-                                                          i,                                        # End idx
-                                                          {"min": numpy.min(thds[prev_idx:i]),      # Other fields
+                    anomaly = mongo.make_anomaly_document(event_id,  # Event id
+                                                          box_id,  # box id
+                                                          mongo.BoxEventType.THD.value,  # Event name
+                                                          "unknown",  # Location
+                                                          0,  # Start ts ms
+                                                          0,  # End ts ms
+                                                          0,  # Duration ms
+                                                          prev_idx,  # Start idx
+                                                          i,  # End idx
+                                                          {"min": numpy.min(thds[prev_idx:i]),  # Other fields
                                                            "max": numpy.max(thds[prev_idx:i]),
                                                            "avg": numpy.average(thds[prev_idx:i])})
                     self.mongo_client.anomalies_collection.insert_one(anomaly)
@@ -133,6 +169,95 @@ class ThdPlugin(plugins.base.MaukaPlugin):
         """
         event_id, box_id, waveform = pickle.loads(message)
         self.debug("Calculating THD for event {} and box {} with waveform of len {}".format(event_id, box_id,
-                                                                                                       len(waveform)))
+                                                                                            len(waveform)))
         self.sliding_thd(event_id, box_id, waveform)
 
+
+###### Testing
+def sliding_thd(event_id: int, box_id: str, waveform: numpy.ndarray):
+    print("a")
+    window_size = int(constants.SAMPLE_RATE_HZ * (200 / 1000.0))
+    windows = rolling_window(waveform, window_size)
+    print("b")
+    # thds = numpy.apply_along_axis(thd, 1, windows)
+    thds = [thd(w) for w in windows]
+    # vthd = numpy.vectorize(thd)
+    # thds = vthd(windows)
+    print("c")
+    prev_beyond_threshold = False
+    prev_idx = -1
+    for i in range(len(thds)):
+        if thds[i] > 5.0:
+            # We only care if this is the start of a new anomaly
+            if not prev_beyond_threshold:
+                prev_idx = i
+                prev_beyond_threshold = True
+        else:
+            # We only care if this is the end of an anomaly
+            if prev_beyond_threshold:
+                anomaly = mongo.make_anomaly_document(event_id,  # Event id
+                                                      box_id,  # box id
+                                                      mongo.BoxEventType.THD.value,  # Event name
+                                                      "unknown",  # Location
+                                                      0,  # Start ts ms
+                                                      0,  # End ts ms
+                                                      0,  # Duration ms
+                                                      prev_idx,  # Start idx
+                                                      i,  # End idx
+                                                      {"min": numpy.min(thds[prev_idx:i]),  # Other fields
+                                                       "max": numpy.max(thds[prev_idx:i]),
+                                                       "avg": numpy.average(thds[prev_idx:i])})
+                print(anomaly)
+                # self.mongo_client.anomalies_collection.insert_one(anomaly)
+                prev_beyond_threshold = False
+
+    return thds
+
+
+import math
+
+
+class WaveformGenerator:
+    def __init__(self):
+        self.amplitude: float = 120.0 * math.sqrt(2)
+        self.frequency: float = 60.0
+        self.phase: float = 0.0
+        self.sample_rate_hz: float = 12000.0
+        self.samples_per_cycle: int = 200
+        self.generator: typing.Generator[typing.Tuple[int, float]] = self.waveform_gen()
+
+    def waveform_gen(self, start: int = 0):
+        i = start
+        while True:
+            v = self.amplitude * math.sin(self.frequency * (2 * math.pi) * i / self.sample_rate_hz)
+            yield (i, v)
+
+            i += 1
+            if i == self.samples_per_cycle:
+                i = 0
+
+    def next(self) -> typing.Tuple[int, float]:
+        return next(self.generator)
+
+    def realize(self, nitems: int):
+        return [self.next() for i in range(nitems)]
+
+
+def extract_waveform(tuple2: typing.Tuple[int, float]) -> float:
+    return tuple2[1]
+
+
+import time
+
+if __name__ == "__main__":
+    gen = WaveformGenerator()
+    # 10 sec of waveform
+    print("warmup")
+    # waveform = numpy.array(list(map(extract_waveform, gen.realize(12000 * 60 * 10))))
+    waveform = numpy.array(list(map(extract_waveform, gen.realize(12000 * 5))))
+    print("start")
+    start = time.time()
+    v = sliding_thd(0, "0", waveform)[:100]
+    end = time.time()
+    print("end", end - start)
+    print(v)
