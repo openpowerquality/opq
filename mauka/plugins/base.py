f8154e6c08b2f5c34412fd1794fa74fe824e2d5e
anthonyjchriste <achriste@hawaii.edu>
2018-05-31 11:03:57 -1000

    Improve debug formatting.

--- mauka/plugins/base.py
+++ mauka/plugins/base.py
@@ -233,7 +233,7 @@ class MaukaPlugin:
 
     def debug(self, msg: str):
         if self.mauka_debug:
-            self.logger.debug("{}: {}".format(self.name, msg))
+            self.logger.debug("{}\n{}".format(self.name, msg))
 
     def _run(self):
         """This is the run loop for this plugin process"""
