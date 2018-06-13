b29a27351184c929798b92080aa3a3c8486eb044
anthonyjchriste <achriste@hawaii.edu>
2018-06-12 14:51:07 -1000

    Update backup script.

--- util/mongod/backup-mongodb.sh
+++ util/mongod/backup-mongodb.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-DATE_STR=`/bin/date +%d%b%Y`
+DATE_STR=`/bin/date +%Y-%m-%d`
 ARCHIVE=opq.dump.${DATE_STR}.tar.gz
-
-# Delete any old backups
-rm -rf /var/opq/backup/mongodb/opq
+BACKUPS_DIR=/home/opquser/backups
 
 # Dump the database
-/usr/local/bin/mongodump --db opq --gzip --out /var/opq/backup/mongodb
+/usr/local/bin/mongodump --db opq --gzip --out ${BACKUPS_DIR}
 
 # Compress the output
-cd /var/opq/backup/mongodb
+cd ${BACKUPS_DIR}
 /bin/tar czf ${ARCHIVE} opq/
 
-# Upload to google drive
-/usr/local/bin/gdrive upload --parent "1K6S8pmlUt6Cc5CWNs_PJyikhdQ9_KXDS" ${ARCHIVE} && rm ${ARCHIVE}
+# Delete the uncompressed output
+rm -rf opq/
+
+## Upload to google drive
+#/usr/local/bin/gdrive upload --parent "1K6S8pmlUt6Cc5CWNs_PJyikhdQ9_KXDS" ${ARCHIVE} && rm ${ARCHIVE}
