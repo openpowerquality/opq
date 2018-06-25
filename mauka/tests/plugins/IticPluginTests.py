53029040077d6c8cfca83247d7caf13739bb2641
anthonyjchriste <achriste@hawaii.edu>
2018-06-25 09:53:37 -1000

    Tests for all regions of ITIC plugins.

--- mauka/tests/plugins/IticPluginTests.py
+++ mauka/tests/plugins/IticPluginTests.py
@@ -68,8 +68,8 @@ class IticPluginTests(unittest.TestCase):
 
     def test_itic_region_no_damange(self):
         """
-        All test results should return a prohibited ITIC region. First we'll test a few that are clearly prohibited
-        and then we will probe around the edge of the prohibited region to test edge cases.
+        All test results should return a no damage ITIC region. First we'll test a few that are clearly no damange
+        and then we will probe around the edge of the no damage region to test edge cases.
         """
         self.assertEqual(IticRegion.NO_DAMAGE, itic_region(percent_nominal_to_rms(40), 500))
         self.assertEqual(IticRegion.NO_DAMAGE, itic_region(percent_nominal_to_rms(40), 1000000))
@@ -83,8 +83,27 @@ class IticPluginTests(unittest.TestCase):
         self.assertEqual(IticRegion.NO_DAMAGE, itic_region(percent_nominal_to_rms(0), 1000000))
 
     def test_itic_region_no_interruption(self):
-        pass
-
+        """
+        All test results should return a no interruption ITIC region. First we'll test a few that are clearly
+        no damage and then we will probe around the edge of the no damage region to test edge cases.
+        """
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(100), 0))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(100), 1))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(100), 10000))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(100), 1000000))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(500), analysis.c_to_ms(0.01)))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(1000), analysis.c_to_ms(0.01)))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(0, analysis.c_to_ms(0.01)))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(0, 20))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(0, 20))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(200), .9))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(140), 2.9))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(120), 2.9))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(110), 0.5))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(90), 9999))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(80), 499))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(70), 19))
+        self.assertEqual(IticRegion.NO_INTERRUPTION, itic_region(percent_nominal_to_rms(40), 19))
 
 
 
