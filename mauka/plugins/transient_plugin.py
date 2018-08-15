d3bb7659bd2551e3c3531b43d987b3188c3cf93d
Charles Dickens <charlieadickens@gmail.com>
2018-08-15 11:41:56 -1000

    implemented method to classify multiple transients in the same event
    implemented method to classify multiple transients in the same event

--- mauka/plugins/transient_plugin.py
+++ mauka/plugins/transient_plugin.py
@@ -14,6 +14,41 @@ from scipy import optimize
 from scipy import stats
 
 
+def transient_sliding_window(filtered_waveform: numpy.ndarray, noise_floor: float, max_lull_ms: float) -> list:
+    """
+    Function to find candidate transient window start and stop indices
+    :param filtered_waveform:
+    :param noise_floor:
+    :param max_lull_ms:
+    :return:
+    """
+    voltage_above_noise_floor = filtered_waveform >= noise_floor
+    transient_windows = []
+    max_samples = int(max_lull_ms / constants.SAMPLES_PER_MILLISECOND)
+    lull_counter = 0
+    new_pass = True
+    start = 0
+
+    for i in range(len(filtered_waveform)):
+        if voltage_above_noise_floor[i]:
+            lull_counter = 0
+            if new_pass:
+                start = i
+                new_pass = False
+        elif not new_pass:
+            lull_counter += 1
+
+        if lull_counter > max_samples:
+            transient_windows.append((start, i - max_samples))
+            lull_counter = 0
+            new_pass = True
+
+    if not new_pass:
+        transient_windows.append((start, len(filtered_waveform)))
+
+    return transient_windows
+
+
 def energy(waveform: numpy.ndarray) -> float:
     """
     Function to calculate the energy of a waveform
@@ -75,14 +110,13 @@ def waveform_filter(raw_waveform: numpy.ndarray, fundamental_freq: float = const
             "raw_waveform": raw_waveform}
 
 
-def oscillatory_classifier(filtered_waveform: numpy.ndarray, configs: dict) -> (bool, dict):
+def oscillatory_classifier(filtered_waveform: numpy.ndarray) -> (bool, dict):
     """
     Identifies whether the transient is oscillatory and, if so, further classifies the transient as a medium, low, or
     high frequency oscillatory transient and calculates additional meta data for the transient such as the magnitude,
     duration, and spectral content.
     :param filtered_waveform: The transient waveform, that is the sampled waveform without the fundamental frequency
     included
-    :param configs: Includes the necessary parameters needed to classify the transient
     :return: A tuple which has contains a boolean indicator of whether the transient was indeed classified as being
     oscillatory and then a dictionary of the calculated meta data.
     """
@@ -102,13 +136,13 @@ def oscillatory_classifier(filtered_waveform: numpy.ndarray, configs: dict) -> (
         :param args: A list containing in this order: guess_amp, guess_freq, guess_phase, guess_mean.
         :return: Optimized function.
         """
-        y_hat = args[0] * numpy.exp(-1.0 * args[1] * idx)  * numpy.cos(args[2] * 2 * numpy.pi * idx + args[3]) + args[4]
+        y_hat = args[0] * numpy.exp(-1.0 * args[1] * idx) * numpy.cos(args[2] * 2 * numpy.pi * idx + args[3]) + args[4]
         return filtered_waveform - y_hat
 
     lst_sq_sol = optimize.leastsq(optimize_func,
                                   numpy.array([guess_amp, guess_decay, guess_freq, guess_phase, guess_mean]))
 
-    RSS1 = numpy.power(optimize_func(lst_sq_sol[0]), 2).sum()
+    rss1 = numpy.power(optimize_func(lst_sq_sol[0]), 2).sum()
 
     def optimize_func_null(args):
         """
@@ -121,18 +155,18 @@ def oscillatory_classifier(filtered_waveform: numpy.ndarray, configs: dict) -> (
 
     lst_sq_sol_null = optimize.leastsq(optimize_func_null, numpy.array([0]))
 
-    RSS0 = numpy.power(optimize_func_null(lst_sq_sol_null[0]), 2).sum()
+    rss0 = numpy.power(optimize_func_null(lst_sq_sol_null[0]), 2).sum()
 
-    Fnum = numpy.divide((RSS0 - RSS1), 4)
-    Fdenom = numpy.divide(RSS1, (len(idx) - 5))
+    f_num = numpy.divide((rss0 - rss1), 4)
+    f_denom = numpy.divide(rss1, (len(idx) - 5))
 
-    F = Fnum / Fdenom
+    f = f_num / f_denom
 
-    p_value = 1 - stats.f.cdf(F, 4, 195)
+    p_value = 1 - stats.f.cdf(f, 4, 195)
 
     if p_value < alpha:
         magnitude = filtered_waveform.max()
-        return True, {'Magnitude': magnitude, 'Frequency': lst_sq_sol[0][2]}
+        return True, {'Magnitude': magnitude, 'Frequency': lst_sq_sol[0][2], 'p_value': p_value}
     else:
         return False, {}
 
@@ -175,6 +209,7 @@ def impulsive_classifier(filtered_waveform: numpy.ndarray, configs: dict) -> (bo
                   "Decay_Time": decay_time, "Start_Index": start_index, "Peak_Index": peak_index,
                   "End_Index": end_index}
 
+
 def arcing_classifier(filtered_waveform: numpy.ndarray, configs: dict) -> (bool, dict):
     """
     Identifies whether the transient is arcing and, if so, calculates additional meta data for the transient, such as
@@ -191,8 +226,8 @@ def arcing_classifier(filtered_waveform: numpy.ndarray, configs: dict) -> (bool,
     num_cycles = transient_zero_xings.sum()
 
     if num_cycles >= 10:
-         if numpy.unique(numpy.diff(numpy.where(transient_zero_xings)), return_counts=True)[1].max() > 2:
-             return True, {'Num_Cycles': num_cycles}
+        if numpy.unique(numpy.diff(numpy.where(transient_zero_xings)), return_counts=True)[1].max() > 2:
+            return True, {'Num_Cycles': num_cycles}
 
     return False, {}
 
@@ -242,11 +277,11 @@ def multiple_zero_xing_classifier(waveforms: dict, configs: dict) -> (bool, dict
         num_xings = extra_xings_above_noise_floor.sum()
         return num_xings > 0, {'num_extra_zero_crossings': num_xings}
     else:
-        return False, None
+        return False, {}
 
 
-def transient_incident_classifier(event_id: int, box_id: str, raw_waveform: numpy.ndarray,
-                                  box_event_start_ts: int, configs: dict):
+def transient_incident_classifier(event_id: int, box_id: str, raw_waveform: numpy.ndarray, box_event_start_ts: int,
+                                  configs: dict, opq_mongo_client: mongo.OpqMongoClient = None, logger=None):
     """
     Classifies transient waveform. Creates a Mongo Incident document
     :param event_id:
@@ -254,16 +289,72 @@ def transient_incident_classifier(event_id: int, box_id: str, raw_waveform: nump
     :param raw_waveform:
     :param box_event_start_ts:
     :param configs:
-    :return:
+    :param opq_mongo_client:
+    :param logger:
+    :return: list of the classified incidents
     """
 
-
+    mongo_client = mongo.get_default_client(opq_mongo_client)
 
     incidents = []
+    meta = {}
+    incident_classifications = []
+    incident_flag = False
 
     waveforms = waveform_filter(raw_waveform)
+    candidate_transient_windows = transient_sliding_window(waveforms["filtered_waveform"], configs["noise_floor"],
+                                                           configs["max_lull_ms"])
 
+    if logger is not None:
+        logger.debug("Calculating transients with {} segments.".format(len(candidate_transient_windows)))
 
+    for window in candidate_transient_windows:
+        windowed_waveforms = {"fundamental_waveform": waveforms["fundamental_waveform"][window[0]: window[1] + 1],
+                              "filtered_waveform": waveforms["filtered_waveform"][window[0]: window[1] + 1],
+                              "raw_waveform": waveforms["raw_waveform"][window[0]: window[1] + 1]}
+        incident_start_ts = int(window[0] / constants.SAMPLES_PER_MILLISECOND + box_event_start_ts)
+        incident_end_ts = int(incident_start_ts + (window[1] - window[0]) / constants.SAMPLES_PER_MILLISECOND)
+
+        impulsive = impulsive_classifier(windowed_waveforms["filtered_waveform"], configs)
+        if impulsive[0]:
+            meta.update(impulsive[1])
+            incident_classifications.append("IMPULSIVE_TRANSIENT")
+            incident_flag = True
+
+        else:
+            arcing = arcing_classifier(windowed_waveforms["filtered_waveform"], configs)
+            if arcing[0]:
+                meta.update(arcing[1])
+                incident_classifications.append("ARCING_TRANSIENT")
+                incident_flag = True
+
+            else:
+                oscillatory = oscillatory_classifier(windowed_waveforms["filtered_waveform"])
+                if oscillatory[0]:
+                    meta.update(arcing[1])
+                    incident_classifications.append("OSCILLATORY_TRANSIENT")
+                    incident_flag = True
+
+        multiple_zero_xing = multiple_zero_xing_classifier(waveforms["filtered_waveform"][window[0]: window[1] + 1],
+                                                           configs)
+        if multiple_zero_xing[0]:
+            meta.update(multiple_zero_xing[1])
+            incident_classifications.append("MULTIPLE_ZERO_CROSSING_TRANSIENT")
+            incident_flag = True
+
+        if incident_flag:
+            incidents.append({"event_id": event_id, "box_id": box_id, "incident_start_ts": incident_start_ts,
+                              "incident_end_ts": incident_end_ts,
+                              "incident_type": mongo.IncidentMeasurementType.TRANSIENT,
+                              "max_deviation": numpy.max(numpy.abs(windowed_waveforms["filtered_waveform"])),
+                              "incident_classifications": incident_classifications,
+                              "annotations": [],
+                              "metadata": meta,
+                              "mongo_client": mongo_client})
+
+        incident_flag = False
+
+    return incidents
 
 
 class TransientPlugin(plugins.base_plugin.MaukaPlugin):
@@ -288,7 +379,8 @@ class TransientPlugin(plugins.base_plugin.MaukaPlugin):
             "pf_cap_switch_low_ratio": float(self.config_get("plugins.TransientPlugin.PF.cap.switching.low.ratio")),
             "pf_cap_switch_high_ratio": float(self.config_get("plugins.TransientPlugin.PF.cap.switching.high.ratio")),
             "pf_cap_switch_low_freq": float(self.config_get("plugins.TransientPlugin.PF.cap.switching.low.freq.hz")),
-            "pf_cap_switch_high_freq": float(self.config_get("plugins.TransientPlugin.PF.cap.switching.high.freq.hz"))
+            "pf_cap_switch_high_freq": float(self.config_get("plugins.TransientPlugin.PF.cap.switching.high.freq.hz")),
+            "max_lull_ms": float(self.config_get("plugins.TransientPlugin.max.lull.ms"))
             }
 
     def on_message(self, topic, mauka_message):
