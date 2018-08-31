7b805746beea3708cb57a837a2eb937f873cd80b
anthonyjchriste <achriste@hawaii.edu>
2018-08-30 15:44:31 -1000

    Mauka: Fix run script for mauka cli.

--- mauka/deploy/mauka-cli.sh
+++ mauka/deploy/mauka-cli.sh
@@ -3,4 +3,4 @@
 # Convenience shortcut for accessing mauka command line interface
 
 cd /usr/local/bin/opq/mauka
-python3 -m plugins.manager /etc/opq/mauka/config.json
+python3 -m services.plugin_manager /etc/opq/mauka/config.json
