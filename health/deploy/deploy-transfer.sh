78a550a5e0ffc5d80fcd71ef29f96109f4b8ed40
anthonyjchriste <achriste@hawaii.edu>
2018-10-05 14:10:23 -1000

    Update deployment scripts to work on OSX

--- health/deploy/deploy-transfer.sh
+++ health/deploy/deploy-transfer.sh
@@ -6,7 +6,8 @@ mkdir $timestamp
 cp deploy-run.sh $timestamp
 cp ../health.py $timestamp
 cp ../config.json $timestamp
-cp ../protobuf/ $timestamp -r
+mkdir -p $timestamp/protobuf
+cp ../protobuf/* $timestamp/protobuf/.
 tar czf $timestamp.tar.gz $timestamp
 rm -rf $timestamp
 scp -P 29862 $timestamp.tar.gz opquser@emilia.ics.hawaii.edu:health
