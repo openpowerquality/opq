a08725981d838b28ba50a676f2d02ceaa9230445
Il Ung Jeong <ilungj@gmail.com>
2018-11-13 04:39:08 -1000

    Send JSON dump that contains OPQ box info

--- makai/health/health.cpp
+++ makai/health/health.cpp
@@ -6,6 +6,7 @@
 #include <unordered_set>
 #include "mongoose.h"
 #include "opqbox3.pb.h"
+#include "json.hpp"
 
 using namespace std::string_literals;
 
@@ -35,30 +36,41 @@ inline uint64_t chrono_to_mili_now() {
 }
 
 std::unordered_set<int> set;
-//SynchronizedMap<int, bool> map;
 
 int main (int argc, char **argv) {
 
 	// Starts the thread that listens to HTTP requests
-	std::thread th{http_server, 8000};
+	std::thread th_http_server{http_server, 8000};
+
+	// Starts the thread that monitors OPQ boxes
+	std::thread th_box_monitor{box_monitor};
 
 	// For good measure
-	th.join();
+	th_http_server.join();
+	th_box_monitor.join();
 
 	return 0;
 }
 
 void ev_handler(struct mg_connection *c, int ev, void *p) {
+
 	if (ev == MG_EV_HTTP_REQUEST) {
+
 		// Initialize push and sub socket
 		zmqpp::context ctx;
 		zmqpp::socket push{ctx, zmqpp::socket_type::push};
 		zmqpp::socket sub{ctx, zmqpp::socket_type::subscribe};
 
+		// TODO Get these interfaces from config
 		push.bind("tcp:://*:9884");
 		sub.bind("tcp://*:9899");
 
+		// Array with all the box info
+		nlohmann::json r = nlohmann::json::array();
+
 		for (auto it = set.begin(); it != set.end(); ++it) {
+
+			// Build Command and send it to the AcquisitionBroker
 			opq::proto3::GetInfoCommand gic;
 			opq::proto3::Command cmd;
 
@@ -72,15 +84,40 @@ void ev_handler(struct mg_connection *c, int ev, void *p) {
 			push.send(out);
 
 			// Listen for response
-			zmqpp::message msg;
-			sub.receive(msg);
+			std::string in;
+			sub.receive(in);
+			
+			// Deserialize message--should be Response
+			opq::proto3::Response res;
+			res.ParseFromString(in);
+
+			// Build JSON (what we want to send)
+			nlohmann::json j;
+			j["box_id"] = res.box_id();
+			j["seq"] = res.seq();
+			j["timestamp_ms"] = res.timestamp_ms();
+
+			// Do we have more info?
+			if (res.has_info_response()) {
+				opq::proto3::GetInfoResponse gir = res.info_response();
+				j["mac_addr"] = gir.mac_addr();
+				j["wifi_network"] = gir.wifi_network();
+				j["ip"] = gir.ip();
+				j["uptime"] = gir.uptime();
+				j["calibration_constant"] = gir.calibration_constant();
+				j["pub_key"] = gir.pub_key();
+				j["measurement_rate"] = gir.measurement_rate();
+			}
+
+			r.push_back(j);
 		}
 
 
+		// Respond to HTTP request
 		struct http_message *hm = (struct http_message *) p;
 
 		mg_send_head(c, 200, hm->message.len, "Content-Type: text/plain");
-		mg_printf(c, "%.*s", (int)hm->message.len, hm->message.p);
+		mg_printf(c, "%s", r.dump());
 	}	
 }
 
@@ -89,6 +126,8 @@ void http_server(int port) {
 	struct mg_connection *c;
 	
 	mg_mgr_init(&mgr, NULL);
+
+	// TODO Use the param port
 	c = mg_bind(&mgr, "8000", ev_handler);
 	if (c == NULL) {
 		std::cout << "Could not connect to port " << "8000"  << std::endl;
@@ -104,11 +143,13 @@ void http_server(int port) {
 
 
 void box_monitor() {
+
 	// Initialize sub socket
 	zmqpp::context ctx;
 	zmqpp::socket sub{ctx, zmqpp::socket_type::subscribe};
-	sub.bind("tcp://*:9881");
 
+	// TODO Get the port from config
+	sub.bind("tcp://*:9881");
 
 	// Wait for socket to bind
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -123,5 +164,6 @@ void box_monitor() {
 		set.insert(box_id);
 	}
 
+	// TODO Get the port from config
 	sub.disconnect("tcp://*:9881");
 }
