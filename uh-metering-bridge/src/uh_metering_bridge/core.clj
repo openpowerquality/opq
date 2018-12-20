8e71f08a9a65f4b9bc6a79be19d583e5b4909f85
anthonyjchriste <achriste@hawaii.edu>
2018-12-19 16:00:55 -1000

    Remove start code from core namespace.

--- uh-metering-bridge/src/uh_metering_bridge/core.clj
+++ uh-metering-bridge/src/uh_metering_bridge/core.clj
@@ -7,6 +7,3 @@
   "Starts the HTTP server for accessing UH meter data."
   []
   (server/start-server))
-
-(server/start-server)
-; http://127.0.01:13000/data/POST_MAIN_1/Frequency/1544043600/1544047200
