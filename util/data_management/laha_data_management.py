10777c9e5c04a29694c2ce3671e92ffb9da0d4c3
anthonyjchriste <achriste@hawaii.edu>
2019-01-08 10:44:37 -1000

    Add stubs for laha data management

---
+++ util/data_management/laha_data_management.py
@@ -0,0 +1,26 @@
+def sample_system_metrics():
+    pass
+
+
+def instantaneous_measurement_metrics():
+    total_active_devices = None
+
+
+def aggregate_measurement_metrics():
+    pass
+
+
+def aggregate_trend_metrics():
+    pass
+
+
+def detection_metrics():
+    pass
+
+
+def incident_metrics():
+    pass
+
+
+def phenomena_metrics():
+    pass
