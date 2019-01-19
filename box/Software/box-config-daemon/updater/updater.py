9988437f41161aaee7f5c798dc790a899704ca24
anthonyjchriste <achriste@hawaii.edu>
2019-01-18 14:49:26 -1000

    Fix updater script path.

--- box/Software/box-config-daemon/updater/updater.py
+++ box/Software/box-config-daemon/updater/updater.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 def install_latest(updates_endpoint: str):
-    with open("box_update.log", "w") as fout:
-        subprocess.run(["bash", "updater.sh", updates_endpoint], stdout=fout, stderr=fout)
+    with open("/usr/local/box-config-daemon/updater/box_update.log", "w") as fout:
+        subprocess.run(["bash", "/usr/local/box-config-daemon/updater/updater.sh", updates_endpoint], stdout=fout, stderr=fout)
