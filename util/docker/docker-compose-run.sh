64467a2c387b0f75c71519adf4f353a8d353faa8
David Aghalarpour <aghalarp@hawaii.edu>
2019-01-29 18:03:33 -0500

    Update Docker Compose run script

--- util/docker/docker-compose-run.sh
+++ util/docker/docker-compose-run.sh
@@ -9,7 +9,7 @@
 export MONGO_URL='mongodb://localhost:27017/opq'
 export ROOT_URL='http://localhost'
 export PORT=8888
-export METEOR_SETTINGS=$(cat ./settings.production.json)
+export METEOR_SETTINGS=$(cat ../settings.production.json)
 
 # Startup Docker-Compose. Note: Be sure that docker-compose.yml is same directory as this script.
 docker-compose up -d
