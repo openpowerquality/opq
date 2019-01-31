1589295b93a8f3fae4e96eb64987302f62eadd11
anthonyjchriste <achriste@hawaii.edu>
2019-01-31 11:30:27 -1000

    Add systemd unit file for box-config-daemon

--- box/Software/box-config-daemon/deploy.sh
+++ box/Software/box-config-daemon/deploy.sh
@@ -2,3 +2,5 @@
 
 sudo rm -rf /usr/local/box-config-daemon/*
 sudo cp -R * /usr/local/box-config-daemon/.
+sudo cp box-config-daemon.service /etc/systemd/system/.
+sudo systemctl enable box-config-daemon.service
