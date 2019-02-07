c3d04fc3a1a3977e03f7fa126e55101a7ef7d7f9
anthonyjchriste <achriste@hawaii.edu>
2019-02-07 10:35:35 -1000

    Update integration diagrams for Mauka.

--- mauka/diagrams/generate_diagrams.sh
+++ mauka/diagrams/generate_diagrams.sh
@@ -2,3 +2,4 @@
 
 dot -Tpng mauka_functional_diagram.dot > mauka_functional_diagram.png
 dot -Tpng mauka_class_inheritance_diagram.dot > mauka_class_inheritance_diagram.png
+dot -Tpng mauka_integration_diagram.dot > mauka_integration_diagram.png
