739015a774a706dd7ec9225fdc2f7782e94ef380
Serge <sin8@hawaii.edu>
2019-02-07 15:25:39 -1000

    More boiler plate

--- makai/TriggeringService/plugins/mongo/src/lib.rs
+++ makai/TriggeringService/plugins/mongo/src/lib.rs
@@ -10,48 +10,33 @@ use std::sync::Arc;
 extern crate serde;
 extern crate serde_json;
 
-#[derive(Serialize, Deserialize, Default, Debug)]
-struct PrintPluginSettings{
-    pub print : bool
-}
-
 #[derive(Debug, Default)]
-pub struct PrintPlugin{
-    settings : PrintPluginSettings
+pub struct MongoPlugin{
 }
 
-impl PrintPlugin {
-    fn new() -> PrintPlugin{
-        PrintPlugin{
-            settings : PrintPluginSettings::default()
+impl MongoPlugin {
+    fn new() -> MongoPlugin{
+        MongoPlugin{
         }
     }
 }
 
-impl MakaiPlugin for PrintPlugin {
+impl MakaiPlugin for MongoPlugin {
 
     fn name(&self) -> &'static str  {
-        "Print Plugin"
+        "Mongo Plugin"
     }
 
     fn process_measurement(&mut self, msg: Arc<Measurement>) -> Option<Vec<Command>> {
-        if self.settings.print{
-            println!("{:?}", msg);
-        }
         None
     }
 
     fn on_plugin_load(&mut self, args : String) {
-        let set = serde_json::from_str(&args);
-        self.settings = match set{
-            Ok(s) => {s},
-            Err(e) => {println!("Bad setting fie for plugin {}: {:?}", self.name(), e); PrintPluginSettings::default()},
-        }
     }
 
     fn on_plugin_unload(&mut self) {
-        println!("Print plugin unloaded.")
+        println!("Mongo plugin unloaded.")
     }
 }
 
-declare_plugin!(PrintPlugin, PrintPlugin::new);
+declare_plugin!(MongoPlugin, MongoPlugin::new);
