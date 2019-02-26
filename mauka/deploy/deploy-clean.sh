5400657c159ba6bcae70a47c1c2f2c41cd506aca
anthonyjchriste <achriste@hawaii.edu>
2019-02-26 10:33:06 -1000

    Remove old Mauka deploy scripts. Move other scripts to util directory. Remove mauka diagrams and move to documentation repo.

--- mauka/deploy/deploy-clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -rf *.tar.bz2
--- mauka/diagrams/generate_diagrams.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-dot -Tpng mauka_functional_diagram.dot > mauka_functional_diagram.png
-dot -Tpng mauka_class_inheritance_diagram.dot > mauka_class_inheritance_diagram.png
-dot -Tpng mauka_integration_diagram.dot > mauka_integration_diagram.png
-dot -Tpng mauka_port_diagram.dot > mauka_port_diagram.png
