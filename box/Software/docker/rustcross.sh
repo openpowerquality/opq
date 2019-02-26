70895fd297e05f1b37a452a88eddc1937ff1d63e
anthonyjchriste <achriste@hawaii.edu>
2019-02-26 10:31:03 -1000

    Update Mauka's API documentation.

---
+++ box/Software/docker/rustcross.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run --rm -ti -v ~/Development/opq/:/home/build/builds piersfinlayson/build
--- util/mauka/mauka-docs.sh
+++ util/mauka/mauka-docs.sh
@@ -3,11 +3,15 @@
 cd ../../mauka
 
 echo "Generating static HTML documentation..."
-python3 -m pydoc -w OpqMauka &&
+python3 -m pydoc -w analysis &&
+python3 -m pydoc -w config &&
+python3 -m pydoc -w constants &&
+python3 -m pydoc -w log &&
+python3 -m pydoc -w mongo &&
+python3 -m pydoc -w opq_mauka &&
+
 python3 -m pydoc -w plugins &&
-python3 -m pydoc -w plugins.acquisition_trigger_plugin &&
 python3 -m pydoc -w plugins.base_plugin &&
-python3 -m pydoc -w plugins.frequency_threshold_plugin &&
 python3 -m pydoc -w plugins.frequency_variation_plugin &&
 python3 -m pydoc -w plugins.ieee1159_voltage_plugin &&
 python3 -m pydoc -w plugins.itic_plugin &&
@@ -18,23 +22,12 @@ python3 -m pydoc -w plugins.print_plugin &&
 python3 -m pydoc -w plugins.semi_f47_plugin &&
 python3 -m pydoc -w plugins.status_plugin &&
 python3 -m pydoc -w plugins.thd_plugin &&
-python3 -m pydoc -w plugins.threshold_plugin &&
-python3 -m pydoc -w plugins.voltage_threshold_plugin &&
+python3 -m pydoc -w plugins.transient_plugin &&
 
 python3 -m pydoc -w protobuf.util &&
 
 python3 -m pydoc -w services.brokers &&
 python3 -m pydoc -w services.plugin_manager &&
-
-python3 -m pydoc -w analysis &&
-python3 -m pydoc -w config &&
-python3 -m pydoc -w constants &&
-python3 -m pydoc -w log &&
-python3 -m pydoc -w mongo &&
-python3 -m pydoc -w opq_mauka &&
-
-#echo "Moving files to gitbook..."
-#mv *.html ../gitbook/mauka/apis/. &&
 
 echo "Moving files to 'api_docs'"
 mkdir -p api_docs
