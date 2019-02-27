10245fa41edc4834204285b4fbedc988a0d5f71f
Evan Hataishi <evanhata@hawaii.edu>
2019-02-26 17:24:19 -1000

    Write mongo health server

---
+++ util/mongod/mongo_check/src/main.rs
@@ -0,0 +1,44 @@
+extern crate mongodb;
+#[macro_use]
+extern crate rouille;
+#[macro_use]
+extern crate serde_derive;
+
+use mongodb::{Client, ThreadedClient, ClientOptions};
+
+#[derive(Serialize)]
+struct resp {
+    up: bool
+}
+
+fn main() {
+
+    rouille::start_server("localhost:28420", move |request| {
+        router!(request,
+            (GET) (/) => {
+                let response = resp {
+                    up: mongo_is_up()
+                };
+                rouille::Response::json(&response)
+            },
+            _ => rouille::Response::empty_404()
+        )
+    });
+}
+
+fn mongo_is_up() -> bool {
+    let mut cl_opt = ClientOptions::new();
+    cl_opt.server_selection_timeout_ms =  1000;
+
+    let client = match Client::connect_with_options("localhost", 27017, cl_opt) {
+        Ok(cl) => match cl.database_names() {
+            Ok(_) => true,
+            Err(_) => false
+        },
+        Err(_) => false
+    };
+
+    println!("Client is up: {}", client);
+
+    client
+}
