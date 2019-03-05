0985275fa5a0185d934c00d2daef7f1f53c46c45
anthonyjchriste <achriste@hawaii.edu>
2019-03-05 11:37:24 -1000

    cleanup makai directory and fix docker build script

--- makai/docker/docker-build.sh
+++ makai/docker/docker-build.sh
@@ -5,6 +5,7 @@ mkdir -p makai-build/makai
 cp -r ../AcquisitionBroker makai-build/makai
 cp -r ../TriggeringBroker makai-build/makai
 cp -r ../TriggeringService makai-build/makai
+cp -r ../config makai-build/makai
 cp -r ../../protocol makai-build
 cp run-makai.sh makai-build/makai
 
