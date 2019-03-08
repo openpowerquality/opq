3d1ff76efa8bb963d0802e0cc79f067bb65b30ef
anthonyjchriste <achriste@hawaii.edu>
2019-03-07 14:47:44 -1000

    Working backup script.

--- mongod/backup-mongodb.sh
+++ mongod/backup-mongodb.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# This script will creates a dated dump of the OPQ database.
-
 DATE_STR=`/bin/date +%Y-%m-%d`
-ARCHIVE=opq.dump.${DATE_STR}.tar.gz
-BACKUPS_DIR=/home/opquser/backups
+ARCHIVE=opq.dump.${DATE_STR}.bak
 
-# Dump the database
-/usr/local/bin/mongodump --db opq --gzip --out ${BACKUPS_DIR}
-
-# Compress the output
-cd ${BACKUPS_DIR}
-/bin/tar czf ${ARCHIVE} opq/
-
-# Delete the uncompressed output
-/bin/rm -rf opq/
+# Dump the database. The resulting archive will be in /var/bak on the host machine.do
+docker pull mongo:4.0.5
+docker tag mongo:4.0.5 bak-mongo
+docker run -it -v /var/bak:/bak --net opq-docker_default --link opq-mongo:bak-mongo --rm mongo mongodump --host mongo --db opq --gzip --archive=/bak/${ARCHIVE} --host mongo
