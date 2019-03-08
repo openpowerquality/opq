39ed0b82431e57c881681cd201b3d69e97931523
anthonyjchriste <achriste@hawaii.edu>
2019-03-07 15:16:46 -1000

    Fix extensions

--- mongod/upload-and-prune.sh
+++ mongod/upload-and-prune.sh
@@ -6,6 +6,6 @@ BACKUPS_DIR=/var/bak
 
 cd ${BACKUPS_DIR}
 
-MOST_RECENT=$(ls opq.dump.*.tar.gz | sort | tail -n 1)
+MOST_RECENT=$(ls opq.dump.*.bak | sort | tail -n 1)
 
-/usr/local/bin/gdrive upload --parent "1K6S8pmlUt6Cc5CWNs_PJyikhdQ9_KXDS" ${MOST_RECENT} && /bin/rm -f opq.dump.*.tar.gz
+/usr/local/bin/gdrive upload --parent "1K6S8pmlUt6Cc5CWNs_PJyikhdQ9_KXDS" ${MOST_RECENT} && /bin/rm -f opq.dump.*.bak
