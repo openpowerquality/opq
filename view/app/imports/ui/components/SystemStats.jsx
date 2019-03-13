fd32c7545ff031df142db261197edf7ed68f5517
Philip Johnson <johnson@hawaii.edu>
2019-03-13 12:25:17 -1000

    checkpoint

--- view/app/imports/ui/components/SystemStats.jsx
+++ view/app/imports/ui/components/SystemStats.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Loader, Statistic, Header, Segment, Grid, List} from 'semantic-ui-react';
+import { Loader, Header, Grid } from 'semantic-ui-react';
 import Moment from 'moment';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
@@ -9,7 +9,7 @@ import { SystemStats } from '../../api/system-stats/SystemStatsCollection';
 
 /**
  * Display numbers as strings with three significant digits. Use 'K' and 'M' for thousands and millions.
- * @param num
+ * @param num (defaults to 0)
  * @returns A string with the number formatted.
  */
 function formatted(num = 0) {
@@ -46,48 +46,73 @@ class SystemStatistics extends React.Component {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
+  /**
+   * Render the Title area.
+   * @returns {*}
+   */
+  renderColumnTitles() {
+    return (
+      <Grid container columns="two">
+        <Grid.Column textAlign="center">
+          <Header as='h3'>Today</Header>
+        </Grid.Column>
+        <Grid.Column textAlign="center">
+          <Header as='h3'>To Date</Header>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+
+  /**
+   * Render each of the entity types.
+   * @param name  Incident, Phenomena, etc.
+   * @param today The number of entities today.
+   * @param toDate The total number of entities
+   * @param ttl The time-to-live value.
+   * @returns {*}
+   */
+  renderEntity(name, today, toDate, ttl) {
+    return (
+      <Grid container>
+        <Grid.Row centered columns="two" style={{ padding: 0 }}>
+          <Grid.Column textAlign="center">
+            <Header as='h3' color="blue">{formatted(today)}</Header>
+          </Grid.Column>
+          <Grid.Column textAlign="center">
+            <Header as='h3' color="blue">{formatted(toDate)}</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered columns="one" style={{ padding: 0 }}>
+          <Grid.Column textAlign="center">
+            <b>{name}</b>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered columns="one" style={{ padding: 0 }}>
+          <Grid.Column textAlign="center">
+            TTL: {ttl}
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns="one" style={{ padding: 0 }}>
+          <Grid.Column>
+            <hr />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+
   /** Here's the system stats page. */
   renderPage() { // eslint-disable-line class-methods-use-this
     const stat = this.props.stats[0];
-    const headerStyle = { textAlign: 'center' };
     const footerStyle = { textAlign: 'center', paddingTop: '10px' };
-    const toDates = [
-      { label: 'phenomena', value: formatted(stat.phenomena_count) },
-      { label: 'incidents', value: formatted(stat.incidents_count) },
-      { label: 'events', value: formatted(stat.events_count) },
-      { label: 'trends', value: formatted(stat.trends_count) },
-      { label: 'measurements', value: formatted(stat.measurements_count) },
-    ];
-    const todays = [
-      { label: 'phenomena', value: formatted(stat.phenomena_count_today) },
-      { label: 'incidents', value: formatted(stat.incidents_count_today) },
-      { label: 'events', value: formatted(stat.events_count_today) },
-      { label: 'trends', value: formatted(stat.trends_count_today) },
-      { label: 'measurements', value: formatted(stat.measurements_count_today) },
-    ];
-    const divStyle = { paddingLeft: '10px', paddingRight: '10px' };
-    const divStyle2 = { paddingLeft: '10px', paddingRight: '10px', marginTop: '5px' };
-    const statStyle = { paddingBottom: '10px'};
     return (
       <WidgetPanel title="System Stats" helpText={this.helpText}>
-        <Grid container columns="two">
-          <Grid.Column textAlign="center">
-            <Grid.Row centered><Header as='h3' style={statStyle}>Today</Header></Grid.Row>
-            <Grid.Row centered><Statistic value={todays[0].value} label={todays[0].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={todays[1].value} label={todays[1].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={todays[2].value} label={todays[2].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={todays[3].value} label={todays[3].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={todays[4].value} label={todays[4].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-          </Grid.Column>
-          <Grid.Column textAlign="center">
-            <Grid.Row centered><Header as='h3' style={statStyle}>To Date</Header></Grid.Row>
-            <Grid.Row centered><Statistic value={toDates[0].value} label={toDates[0].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={toDates[1].value} label={toDates[1].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={toDates[2].value} label={toDates[2].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={toDates[3].value} label={toDates[3].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-            <Grid.Row centered><Statistic value={toDates[4].value} label={toDates[4].label} size="mini" color="blue" style={statStyle}/></Grid.Row>
-          </Grid.Column>
-        </Grid>
+        {this.renderColumnTitles()}
+        {this.renderEntity('PHENOMENA', stat.phenomena_count_today, stat.phenomena_count, '?')}
+        {this.renderEntity('INCIDENTS', stat.incidents_count_today, stat.incidents_count, '?')}
+        {this.renderEntity('EVENTS', stat.events_count_today, stat.trends_count, '?')}
+        {this.renderEntity('TRENDS', stat.trends_count_today, stat.trends_count, '?')}
+        {this.renderEntity('MEASUREMENTS', stat.measurements_count_today, stat.measurements_count, '?')}
         <p style={footerStyle}>Last update: {Moment(stat.timestamp).format('MMMM Do YYYY, h:mm:ss a')}</p>
       </WidgetPanel>
     );
