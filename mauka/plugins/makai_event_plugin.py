3fd8ad3c3c8a9c04b8b1a2d54b1696a35fc5e5b6
anthonyjchriste <achriste@hawaii.edu>
2019-03-14 15:02:36 -1000

    Fix calibration constant.

--- mauka/plugins/makai_event_plugin.py
+++ mauka/plugins/makai_event_plugin.py
@@ -185,7 +185,7 @@ def acquire_data(mongo_client: mongo.OpqMongoClient,
     makai_event_plugin.debug("box_id={} and type={}".format(box_id, type(box_id)))
     calibration_constant = makai_event_plugin.mongo_client.get_box_calibration_constant(box_id)
     makai_event_plugin.debug("calibration_constant=%f" % calibration_constant)
-    waveform_calibrated = waveform / mongo.cached_calibration_constant(box_id)
+    waveform_calibrated = waveform / calibration_constant
 
     makai_event_plugin.debug("Loaded calibrated waveform for event_id=%s box_id=%s with len=%d" %
                              (event_id, box_id, len(waveform_calibrated)))
