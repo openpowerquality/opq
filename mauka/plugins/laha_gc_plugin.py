d635fd70e0e9a9b5c50c7e39824335d84238c0e9
anthonyjchriste <achriste@hawaii.edu>
2019-03-21 16:28:16 -1000

    Lay groundwork for GC implemented as plugin.

--- mauka/plugins/laha_gc_plugin.py
+++ mauka/plugins/laha_gc_plugin.py
@@ -1,9 +1,14 @@
 import multiprocessing
+import time
 
 import config
 import plugins.base_plugin as base_plugin
 import protobuf.util as util_pb2
 import protobuf.mauka_pb2 as mauka_pb2
+
+
+def timestamp_s() -> int:
+    return int(round(time.time()))
 
 
 class LahaGcPlugin(base_plugin.MaukaPlugin):
@@ -16,19 +21,37 @@ class LahaGcPlugin(base_plugin.MaukaPlugin):
 
     def handle_gc_trigger_measurements(self):
         self.debug("gc_trigger measurements")
-    
+        now = timestamp_s()
+        delete_result = self.mongo_client.measurements_collection.delete_many({"expires_at": {"$lt": now}})
+        self.debug("Garbage collected %d measurements" % delete_result.deleted_count)
 
     def handle_gc_trigger_trends(self):
         self.debug("gc_trigger trends")
-        pass
+        now = timestamp_s()
+        delete_result = self.mongo_client.trends_collection.delete_many({"expires_at": {"$lt": now}})
+        self.debug("Garbage collected %d trends" % delete_result.deleted_count)
 
     def handle_gc_trigger_events(self):
         self.debug("gc_trigger events")
-        pass
+        now = timestamp_s()
+        events = self.mongo_client.events_collection.find({"expires_at": {"$lt": now}}
+                                                          )
+        # Find corresponding box events
+
+        # Cleanup gridfs files for each box event
+
+        # Cleanup box events
+
+        # Cleanup events
+        delete_result = self.mongo_client.events_collection.delete_many({"expires_at": {"$lt": now}})
+        self.debug("Garbage collected %d events" % delete_result.deleted_count)
+
 
     def handle_gc_trigger_incidents(self):
         self.debug("gc_trigger incidents")
-        pass
+        now = timestamp_s()
+        delete_result = self.mongo_client.incidents_collection.delete_many({"expires_at": {"$lt": now}})
+        self.debug("Garbage collected %d incidents" % delete_result.deleted_count)
 
     def handle_gc_trigger_phenomena(self):
         self.debug("gc_trigger phenomena")
@@ -53,15 +76,45 @@ class LahaGcPlugin(base_plugin.MaukaPlugin):
         if mauka_pb2.PHENOMENA in domains:
             self.handle_gc_trigger_phenomena()
 
-    def handle_gc_update(self, gc_update: mauka_pb2.GcUpdate):
+    def handle_gc_from_phenomena(self, _id: str):
+        self.debug("gc_update phenomena")
         pass
+
+    def handle_gc_from_incident(self, _id: str):
+        self.debug("gc_update incidents")
+        pass
+
+    def handle_gc_from_event(self, _id: str):
+        self.debug("gc_from event")
+        pass
+
+    def handle_gc_from_trend(self, _id: str):
+        self.debug("gc_update trends")
+        pass
+
+    def handle_gc_from_measurement(self, _id: str):
+        self.debug("gc_update measurements")
+        pass
+
+    def handle_gc_update(self, gc_update: mauka_pb2.GcUpdate):
+        if gc_update.from_domain == mauka_pb2.PHENOMENA:
+            self.handle_gc_from_phenomena(gc_update.id)
+        elif gc_update.from_domain == mauka_pb2.INCIDENTS:
+            self.handle_gc_from_incident(gc_update.id)
+        elif gc_update.from_domain == mauka_pb2.EVENTS:
+            self.handle_gc_from_event(gc_update.id)
+        elif gc_update.from_domain == mauka_pb2.TRENDS:
+            self.handle_gc_from_trend(gc_update.id)
+        elif gc_update.from_domain == mauka_pb2.MEASUREMENTS:
+            self.handle_gc_from_measurement(gc_update.id)
+        else:
+            pass
 
     def on_message(self, topic: str, mauka_message: mauka_pb2.MaukaMessage):
         self.debug("Received from %s" % mauka_message.source)
-
         if util_pb2.is_gc_trigger(mauka_message):
             self.handle_gc_trigger(mauka_message.laha.gc_trigger)
         elif util_pb2.is_gc_update(mauka_message):
-            pass
+            self.handle_gc_update(mauka_message.gc_update)
         else:
             self.logger.error("Received incorrect type of MaukaMessage")
