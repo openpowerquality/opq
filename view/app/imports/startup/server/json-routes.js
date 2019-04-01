00b882507e880422658c848a790ca58ba088fb49
Philip Johnson <johnson@hawaii.edu>
2019-04-01 10:54:28 -1000

    Return the timestamp in seconds, not milliseconds.

--- view/app/imports/startup/server/json-routes.js
+++ view/app/imports/startup/server/json-routes.js
@@ -1,12 +1,16 @@
 import { JsonRoutes } from 'meteor/simple:json-routes';
 import moment from 'moment';
 
+/**
+ * Support the Health endpoint.
+ */
+
 JsonRoutes.add('get', '/health', function (req, res) {
   JsonRoutes.sendResult(res, {
     data: {
       name: 'view',
       ok: true,
-      timestamp: moment().valueOf(),
+      timestamp: Math.trunc(moment().valueOf() / 1000),
     },
   });
 });
