f1393b3d36c96f136a14ea4b8a32f3c291293c69
Philip Johnson <johnson@hawaii.edu>
2019-04-01 09:25:11 -1000

    Fix bugs in View w.r.t. default view.config.json

--- view/app/imports/startup/server/synced-cron.js
+++ view/app/imports/startup/server/synced-cron.js
@@ -5,14 +5,14 @@ import { SystemStats } from '../../api/system-stats/SystemStatsCollection.js';
 // This call to SyncedCron.config should exist in only one place. Here! Don't copy this code to other files.
 // Logging defaults to off unless explicitly enabled.
 SyncedCron.config({
-  log: Meteor.settings.syncedCron && Meteor.settings.syncedCron.logging,
+  log: (Meteor.settings.syncedCron && Meteor.settings.syncedCron.logging) || false,
 });
 
 function startupSystemStatsCronjob() {
   // Only set up Cron Job when not in Test mode.
   if (!Meteor.isTest && !Meteor.isAppTest) {
     // Default the update interval to 60 seconds if not supplied in configuration file.
-    const updateIntervalSeconds = Meteor.settings.systemStats.updateIntervalSeconds || 60;
+    const updateIntervalSeconds = Meteor.settings.systemStats ? Meteor.settings.systemStats.updateIntervalSeconds : 60;
     SyncedCron.add({
       name: 'Update the SystemStats collection with current collection counts',
       schedule(parser) {
