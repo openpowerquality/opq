0295fcca6f02d718883aa6cbfc14f414d2088e60
Evan Hataishi <evanhata@hawaii.edu>
2019-04-10 23:04:30 -1000

    Fix various issues

--- health/src/main.rs
+++ health/src/main.rs
@@ -3,30 +3,23 @@ extern crate serde_json;
 extern crate chrono;
 extern crate bson;
 
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::result::Result;
 use reqwest::{Response, Error};
-//use std::collections::HashMap;
 
-// File reading stuff
 use std::fs::File;
-use std::io::BufReader;
-use std::io::Read;
+use std::io::{BufReader, Read};
 
-// json stuff
+
 use serde_json::Value;
 
 use std::thread;
 
-use chrono::{DateTime, Utc};
+use chrono::Utc;
 use bson::*;
-use bson::Document;
-use bson::TimeStamp;
+use bson::{Document, TimeStamp};
 
-use mongodb;
-use mongodb::Client;
-use mongodb::ThreadedClient;
-use mongodb::ClientOptions;
+use mongodb::{Client, ThreadedClient, ClientOptions};
 use mongodb::db::ThreadedDatabase;
 
 #[macro_use]
@@ -50,7 +43,8 @@ pub struct HealthStatus {
     name: String,
     ok: bool,
     timestamp: u64,
-    subcomponents: Vec<HealthStatus>,
+    info: Option<String>,
+    subcomponents: Option<Vec<HealthStatus>>,
 }
 
 struct HealthDoc {
@@ -61,14 +55,18 @@ struct HealthDoc {
     timestamp: TimeStamp,
 }
 
+// TODO - set this from the config file
+static MONGODB_URI: &str = "mongodb://localhost:27017";
+
 fn main() {
     let config = parse_config_file("src/services.json");
 
     loop {
         for service in &config.services {
-            check_service(service, &config.mongodb);
+            query_service(service);
         }
-        thread::sleep_ms((config.interval * 1000) as u32);
+        // IDE highlights error without extra set of parentheses
+        thread::sleep(Duration::from_secs((config.interval)));
     }
 }
 
@@ -116,7 +114,7 @@ fn read_file(filepath: &str) -> String {
     contents
 }
 
-fn check_service(service: &Service, mongodb: &str) {
+fn query_service(service: &Service) {
     let client = reqwest::Client::builder()
         .timeout(Duration::from_secs(2))
         .build().expect("Unable to build client");
@@ -127,65 +125,107 @@ fn check_service(service: &Service, mongodb: &str) {
     let req_error = http_send_err(&req_result);
 
     if req_error != "" {
-        println!("{} {} is DOWN Error: {}", Utc::now(), service.name, req_error);
+        insert_health_doc(&generate_error_health_status(service.name.to_string(), req_error.to_string()));
+        return;
+    }
+
+    let mut response = req_result.unwrap();
+    if response.status() == 200 {
+        match response.json() {
+            Ok(status) => {
+                let stat: HealthStatus = status;
+                check_status(&stat);
+            }
+            Err(_) => {
+                insert_health_doc(&generate_error_health_status(service.name.to_string(), "Unable to parse response".to_string()));
+            }
+        };
     } else {
-        let mut response = req_result.unwrap();
-        if response.status() == 200 {
-            // println!("{} {} is UP", Utc::now(), service.name);
-            match response.json() {
-                Ok(status) => insert_health_doc(status, mongodb),
-                Err(_) => println!("Unable to parse response")
-            };
-        } else {
-            println!("{} {} is DOWN Error: {}", Utc::now(), service.name, response.status());
-        }
+        insert_health_doc(&generate_error_health_status(service.name.to_string(), response.status().to_string()));
     }
 }
 
 // Response may be a non-http error e.g. connection timeout, refused, etc.
-// Why is it so hard to get the error?
-fn http_send_err(response: &Result<Response, Error>) -> &str {
-    let mut error = "";
+fn http_send_err(response: &Result<Response, Error>) -> String {
     match response {
-        Err(e) => {
-            // println!("{:?}", e.get_ref().unwrap());
-            match e.get_ref() {
-                Some(nested_e) => match nested_e.cause() {
-                    Some(nested_e) => {
-                        // println!("{:?}", nested_e.description());
-                        error = nested_e.description()
-                    }
-                    _ => error = "Unable to parse error"
-                },
-                _ => error = "Unable to parse error"
-            }
-            error
-        }
-        Ok(_) => ""
+        Ok(_) => "".to_string(),
+        Err(e) => e.to_string()
     }
 }
 
-fn insert_health_doc(status: HealthStatus, mongodb: &str) {
-    let mut options = ClientOptions::new();
-    options.server_selection_timeout_ms = 1000;
-    let client = Client::with_uri_and_options(mongodb, options)
-        .expect("Can't connect to mongo");
-    let coll = client.db("opq").collection("healthv2");
-    let mut doc = doc! {
-        "service": status.name,
-        "serviceID": "".to_string(),
-        "status": get_status(status.ok),
-        "info": "".to_string(),
+fn check_status(status: &HealthStatus) {
+    println!("{:?}", status);
+    insert_health_doc(status);
+    match &status.subcomponents {
+        Some(subcomponents) => {
+            for component in subcomponents {
+                check_status(component);
+            }
+        }
+        None => ()
+    }
+}
+
+fn generate_health_doc(status: &HealthStatus) -> Document {
+    // Special case for boxes
+    let service: String = {
+        match status.name.to_string().parse::<u64>() {
+            Ok(_) => "BOX".to_string(),
+            Err(_) => status.name.to_string()
+        }
+    };
+    let serviceID: String = {
+        if service == "BOX" {
+            status.name.to_string()
+        } else {
+            "".to_string()
+        }
+    };
+
+    let info: String = {
+        match &status.info {
+            Some(info) => info.to_string(),
+            None => "".to_string()
+        }
+    };
+
+    let doc = doc! {
+        "service": service.to_uppercase(),
+        "serviceID": serviceID.to_uppercase(),
+        "status": get_status(status.ok).to_uppercase(),
+        "info": info,
         "timestamp": Utc::now()
         };
-    coll.insert_one(doc, None).unwrap();
+    doc
+}
+
+fn insert_health_doc(status: &HealthStatus) {
+    println!("{:?}", status);
+    let mut options = ClientOptions::new();
+    options.server_selection_timeout_ms = 1000;
+    let client = Client::with_uri_and_options(&MONGODB_URI, options)
+        .expect("Can't connect to mongo");
+    let coll = client.db("opq").collection("healthv2");
+    // coll.insert_one(generate_health_doc(status), None).unwrap();
 }
 
 fn get_status(up: bool) -> String {
     if up {
-        return "UP".to_string()
+        return "UP".to_string();
     } else {
-        return "DOWN".to_string()
+        return "DOWN".to_string();
     }
+}
+
+// If an error is generated by this health client
+fn generate_error_health_status(name: String, message: String) -> HealthStatus {
+    let status: HealthStatus = HealthStatus {
+        name,
+        ok: false,
+        timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+        info: Some(message),
+        subcomponents: None,
+    };
+    status
 }
 
