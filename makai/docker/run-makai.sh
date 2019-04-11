45b598b0b7dab6320803892f80425efc679d5d7c
Serge <sin8@hawaii.edu>
2019-04-11 13:06:10 -1000

    fixed docker

--- makai/docker/run-makai.sh
+++ makai/docker/run-makai.sh
@@ -2,5 +2,5 @@
 
 /build/bin/AcquisitionBroker ACQUISITION_BROKER_SETTINGS &
 /build/bin/TriggeringBroker TRIGGERING_BROKER_SETTINGS &
-/build/bin/event_service 
+/build/bin/event_service &
 /build/bin/makai
