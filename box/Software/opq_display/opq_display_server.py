5f4128fc5b5716c5a48634916fd06517fc1762b4
anthonyjchriste <achriste@hawaii.edu>
2019-04-23 16:19:58 -1000

    Split out the display client into its own module.

--- box/Software/opq_display/opq_display_server.py
+++ box/Software/opq_display/opq_display_server.py
@@ -13,36 +13,6 @@ logging.basicConfig(
 logger.setLevel(logging.DEBUG)
 
 
-class OpqDisplayClient:
-
-    def __init__(self, port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.addr = ("127.0.0.1", port)
-
-    def send_cmd(self, cmd):
-        if not display_types.is_display_type(cmd):
-            logger.error("Invalid cmd %s, valid commands are %s", cmd, display_types.DISPLAY_TYPES)
-            return
-
-        try:
-            bytes_sent = self.sock.sendto(bytes(cmd), self.addr)
-            logger.debug("Sent cmd %s with %d bytes to server at %s", cmd, bytes_sent, self.addr)
-        except Exception as e:
-            logger.error("Error sending command to server: %s", str(e))
-        finally:
-            self.sock.close()
-            logger.debug("Client connection closed.")
-
-    def send_display_splash_cmd(self):
-        self.send_cmd(display_types.DISPLAY_SPLASH)
-
-    def send_display_normal_cmd(self):
-        self.send_cmd(display_types.DISPLAY_NORMAL)
-
-    def send_display_ap_cmd(self):
-        self.send_cmd(display_types.DISPLAY_AP)
-
-
 def handle_cmd(opq_disp, cmd):
     if not display_types.is_display_type(cmd):
         logger.error("Invalid cmd %s, valid commands are %s", cmd, display_types.DISPLAY_TYPES)
@@ -79,24 +49,8 @@ def start_server(port):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
-
-    parser_server = subparsers.add_parser("server",
-                                          help="Starts the opq-display server")
-    parser_server.add_argument("port", type=int, help="opq-display server port")
-
-    parser_client = subparsers.add_parser("client",
-                                          help="Starts a opq-display client")
-    parser_client.add_argument("port", type=int, help="opq-display server port")
-    parser_client.add_argument("cmd", help="cmd to send to display server")
-
+    parser.add_argument("port",
+                        type=int,
+                        help="Port the display server should run on.")
     args = parser.parse_args()
-
-    if "cmd" in args:
-        port = args.port
-        cmd = args.cmd
-        client = OpqDisplayClient(port)
-        client.send_cmd(cmd)
-    else:
-        port = args.port
-        start_server(port)
+    start_server(args.port)
