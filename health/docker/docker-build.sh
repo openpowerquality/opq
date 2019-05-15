0a616cc148b3e9c052cad9858a29053d250d3d7c
opq <achriste@hawaii.edu>
2019-05-14 15:19:52 -1000

    Update health dockerfication.

--- health/docker/docker-build.sh
+++ health/docker/docker-build.sh
@@ -4,6 +4,7 @@ echo "Building health..."
 mkdir -p build/health
 cp -r ../src build/health
 cp ../Cargo.toml build/health
+cp run-health.sh build/health
 docker build -t healthv2 .
 rm -rf build
 echo "Done building health."
