1c0045d9d629f1b6fc784699887b7f07b5335001
Sergey Negrashov <sin8@hawaii.edu>
2019-05-14 14:38:58 -1000

    changed mime to json

--- makai/TriggeringService/plugins/health/src/http_endpoint.rs
+++ makai/TriggeringService/plugins/health/src/http_endpoint.rs
@@ -4,47 +4,51 @@ use std::sync::Mutex;
 use std::time::{SystemTime, UNIX_EPOCH};
 use types::{HealthPluginSettings, MakaiStatus, Statistics, SERVICE_NAME};
 
+use iron::headers;
+use iron::modifiers::Header;
 use iron::prelude::*;
 use iron::status;
 use iron::Handler;
-
 struct Router {
-    stats: Arc<Mutex<Statistics>>
+    stats: Arc<Mutex<Statistics>>,
 }
 
 impl Router {
     fn new(stats: Arc<Mutex<Statistics>>) -> Self {
-        Router {
-            stats
-        }
+        Router { stats }
     }
 }
 
 impl Handler for Router {
     fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        match req.url.path().join("/").as_str(){
+        match req.url.path().join("/").as_str() {
             "trigger" => {
                 let mut stats = self.stats.lock().unwrap();
                 stats.trigger_now = true;
                 Ok(Response::with((status::ImATeapot, "Triggered")))
-            },
+            }
             "" => {
                 let stats = self.stats.lock().unwrap();
                 let mut status = MakaiStatus::default();
                 status.name = SERVICE_NAME.to_string();
                 status.ok = true;
-                status.timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-                for (_, value) in stats.box_status.iter(){
+                status.timestamp = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs();
+                for (_, value) in stats.box_status.iter() {
                     status.subcomponents.push(value.clone());
                 }
-                Ok(Response::with((status::Ok, serde_json::to_string(&status).unwrap())))
-            },
+                Ok(Response::with((
+                    status::Ok,
+                    Header(headers::ContentType::json()),
+                    serde_json::to_string(&status).unwrap(),
+                )))
+            }
             _ => Ok(Response::with(status::NotFound)),
         }
-
     }
 }
-
 
 pub fn start_server(stats: Arc<Mutex<Statistics>>, settings: HealthPluginSettings) {
     let addr = settings.address.clone();
