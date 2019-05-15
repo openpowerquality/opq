eb9407b360afc5da2d83ee0b0f4583373b2934fe
opq <achriste@hawaii.edu>
2019-05-14 14:51:41 -1000

    Update health dockerfication.

--- health/docker/docker-build.sh
+++ health/docker/docker-build.sh
@@ -1,31 +1,9 @@
+#!/usr/bin/env bash
 
-# Subdirectory for src files
-timestamp=$(date +%Y%m%d_%H%M%S)
-mkdir $timestamp
-
-######################
-# Build Docker Image #
-######################
-
-IMAGE_NAME=healthv2
-
-echo "=> Copying health source files into $timestamp"
-
-cp ../Cargo.toml ./$timestamp/
-cp -r ../src ./$timestamp/
-cp ./Dockerfile ./$timestamp/
-
-cd $timestamp
-
-echo "=> Building Docker image..."
-
-docker build --tag=${IMAGE_NAME} .
-
-echo "=> Deleting all temporary files..."
-
-cd ..
-
-rm -rf $timestamp
-
-echo "=> Docker build complete!"
-echo "=> Use the 'docker image ls' command to check that the '${IMAGE_NAME}' image was successfully generated"
+echo "Building health..."
+mkdir -p build/health
+cp -r ../src build/health
+cp ../Cargo.toml build/health
+docker build -t healthv2 .
+rm -rf build
+echo "Done building health."
