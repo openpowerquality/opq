82e3d76ef93d62165e462e8885edfc796b895169
anthonyjchriste <achriste@hawaii.edu>
2019-05-28 09:34:50 -1000

    Store the client in the handler so that a new client is not created for each request. #161

--- health/src/mongo_service_checker.rs
+++ health/src/mongo_service_checker.rs
@@ -5,11 +5,13 @@ use iron::{mime, status, Handler, Iron, IronError, Request, Response};
 use mongodb;
 use mongodb::db::ThreadedDatabase;
 use mongodb::{Client, ThreadedClient};
+use log;
+
 
 const OPQ_DB: &str = "opq";
 const HEALTH_COLL: &str = "health";
 
-fn mongo_client(mongo_host: &str, mongo_port: u16) -> Option<Client> {
+fn get_mongo_client(mongo_host: &str, mongo_port: u16) -> Option<Client> {
     let mongo_client: mongodb::error::Result<Client> = Client::connect(mongo_host, mongo_port);
     match mongo_client {
         Ok(client) => Some(client),
@@ -18,20 +20,18 @@ fn mongo_client(mongo_host: &str, mongo_port: u16) -> Option<Client> {
 }
 
 struct RequestHandler {
-    mongo_host: String,
-    mongo_port: u16,
+    mongo_client: Option<Client>,
 }
 
 impl RequestHandler {
-    fn new(mongo_host: String, mongo_port: u16) -> RequestHandler {
+    fn new(mongo_host: &str, mongo_port: u16) -> RequestHandler {
         RequestHandler {
-            mongo_host,
-            mongo_port,
+            mongo_client: get_mongo_client(mongo_host, mongo_port),
         }
     }
 
     fn mongo_status(&self) -> bool {
-        match mongo_client(&self.mongo_host, self.mongo_port) {
+        match self.mongo_client.clone() {
             None => false,
             Some(client) => match client.db(OPQ_DB).collection_names(None) {
                 Ok(collection_names) => collection_names.contains(&HEALTH_COLL.to_string()),
@@ -55,7 +55,7 @@ pub fn start_mongo_service_checker(health_config: HealthConfig) {
     let builder = thread::Builder::new().name("mongo_service_checker".to_string());
     let join_handle = builder
         .spawn(move || {
-            let handler = RequestHandler::new(health_config.mongo_host, health_config.mongo_port);
+            let handler = RequestHandler::new(&health_config.mongo_host, health_config.mongo_port);
             let server = Iron::new(handler)
                 .http(health_config.mongo_service_checker_addr)
                 .expect("Error starting mongo server checker server");
