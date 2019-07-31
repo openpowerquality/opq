7b898ad7e86ae52f0ad83fc20771409fdc2d6c24
opq <achriste@hawaii.edu>
2019-07-30 14:16:32 -1000

    Update backup-mongodb script to work on new host.

--- mongod/upload-prune-backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script first uploads the latest local backup and then deletes all local backups. Then, this script creates a new
-# local backup. This allows us to always have one local backup on the server and the previous day's backup
-# "in the cloud".
-
-. /home/opquser/backup/upload-and-prune.sh && . /home/opquser/backup-mongodb.sh
