a0342476203f6c4e4c48bbcc8e8f3d9a8982d41d
opq <achriste@hawaii.edu>
2019-09-05 10:28:45 -1000

    Fix logging for ITIC plugin

--- mauka/docker/build/mauka/protobuf/build_proto.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-protoc --python_out=. -I ../../protocol/ mauka.proto opqbox3.proto rustproto.proto
-
-# Update opqbox3_pb2.py to import the rustproto_pb2 source from the correct location.
-sed -i 's/import rustproto_pb2 as rustproto__pb2/import protobuf.rustproto_pb2 as rustproto__pb2/' opqbox3_pb2.py
