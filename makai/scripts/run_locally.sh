dff77c416753e971175e91336c41b91a4c6b25b2
anthonyjchriste <achriste@hawaii.edu>
2019-09-18 11:18:04 -1000

    Add kill script for local makai.

--- makai/scripts/run_locally.sh
+++ makai/scripts/run_locally.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAKAI_CONFIG_DIR=/etc/opq/makai
-
+export RUST_BACKTRACE=1
 /usr/local/bin/ab ${MAKAI_CONFIG_DIR}/acquisition_broker.config.json &
 /usr/local/bin/tb ${MAKAI_CONFIG_DIR}/triggering_broker.config.json &
 /usr/local/bin/event_service ${MAKAI_CONFIG_DIR}/makai.config.json &
