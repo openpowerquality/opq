d9e80e5a1af3f442a0db4936f156e2834e0162be
anthonyjchriste <achriste@hawaii.edu>
2019-09-18 10:54:59 -1000

    Add install script for makai.

--- makai/scripts/install_locally.sh
+++ makai/scripts/install_locally.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 cd ..                                                                                                          && \
-MAKAI_BIN=/usr/local/bin/makai                                                                                 && \
+MAKAI_BIN=/usr/local/bin                                                                                       && \
 echo "Installing Makai binaries to ${MAKAI_BIN}"                                                               && \
 mkdir -p                                                                                        ${MAKAI_BIN}   && \
-cp AcquisitionBroker/target/release/ab                                                          ${MAKAI_BIN}/. && \
-cp EventService/target/release/event_service                                                    ${MAKAI_BIN}/. && \
-cp TriggeringBroker/target/release/tb                                                           ${MAKAI_BIN}/. && \
-cp TriggeringService/target/release/makai                                                       ${MAKAI_BIN}/. && \
-cp TriggeringService/plugins/health/target/release/libhealth_plugin.so                          ${MAKAI_BIN}/. && \
-cp TriggeringService/plugins/napali_trigger/target/release/libnapali_plugin.so                  ${MAKAI_BIN}/. && \
-cp TriggeringService/plugins/print/target/release/libprint_plugin.so                            ${MAKAI_BIN}/. && \
-cp TriggeringService/plugins/threshold_trigger/target/release/libthreshold_trigger_plugin.so    ${MAKAI_BIN}/.
+cp AcquisitionBroker/target/release/ab                                                          ${MAKAI_BIN}   && \
+cp EventService/target/release/event_service                                                    ${MAKAI_BIN}   && \
+cp TriggeringBroker/target/release/tb                                                           ${MAKAI_BIN}   && \
+cp TriggeringService/target/release/makai                                                       ${MAKAI_BIN}   && \
+cp TriggeringService/plugins/health/target/release/libhealth_plugin.so                          ${MAKAI_BIN}   && \
+cp TriggeringService/plugins/napali_trigger/target/release/libnapali_plugin.so                  ${MAKAI_BIN}   && \
+cp TriggeringService/plugins/print/target/release/libprint_plugin.so                            ${MAKAI_BIN}   && \
+cp TriggeringService/plugins/threshold_trigger/target/release/libthreshold_trigger_plugin.so    ${MAKAI_BIN}
 
