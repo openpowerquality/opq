61ba8e0f38e0af6a9dd2dab85ad48c932f8804fb
anthonyjchriste <achriste@hawaii.edu>
2019-09-18 10:59:19 -1000

    Add run script for local makai.

---
+++ makai/scripts/run_locally.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MAKAI_CONFIG_DIR=/etc/opq/makai
+
+/usr/local/bin/ab ${MAKAI_CONFIG_DIR}/acquisition_broker.config.json &
+/usr/local/bin/tb ${MAKAI_CONFIG_DIR}/triggering_broker.config.json &
+/usr/local/bin/event_service ${MAKAI_CONFIG_DIR}/makai.json &
+/usr/local/bin/makai ${MAKAI_CONFIG_DIR}/makai.json
