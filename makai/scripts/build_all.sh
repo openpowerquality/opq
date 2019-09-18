550acf1935248ed1bde13537e579039be8483316
anthonyjchriste <achriste@hawaii.edu>
2019-09-18 09:55:18 -1000

    Add build script for Makai.

--- makai/scripts/build_all.sh
+++ makai/scripts/build_all.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
-export RUSTC_WRAPPER=~/.cargo/bin/sccache
-sccache --start-server
-
-echo "Building AcquisitionBroker"
-cd ../AcquisitionBroker
-cargo build --release
-
-echo "Building EventService"
-cd ../EventService
-cargo build --release
-
-echo "Building TriggeringBroker"
-cd ../TriggeringBroker
-cargo build --relase
-
-echo "Building TriggeringService"
-cd ../TriggeringService
-cargo build --release
-
-echo "Building TriggeringService plugin health"
-cd plugins/health
-cargo build --release
-
-echo "Building TriggeringService plugin mongo"
-cd ../mongo
-cargo build --release
-
-echo "Building TriggeringService plugin napali_trigger"
-cd ../napali_trigger
-cargo build --release
-
-echo "Building TriggeringService plugin print"
-cd ../print
-cargo build --release
-
-echo "Building TriggeringService plugin threshold_trigger"
-cd ../threshold_trigger
-cargo build --release
-
+export RUSTC_WRAPPER=~/.cargo/bin/sccache && \
+sccache --start-server && \
+echo "Building AcquisitionBroker" && \
+cd ../AcquisitionBroker && \
+cargo build --release && \
+echo "Building EventService" && \
+cd ../EventService && \
+cargo build --release && \
+echo "Building TriggeringBroker" && \
+cd ../TriggeringBroker && \
+cargo build --release && \
+echo "Building TriggeringService" && \
+cd ../TriggeringService && \
+cargo build --release && \
+echo "Building TriggeringService plugin health" && \
+cd plugins/health && \
+cargo build --release && \
+echo "Building TriggeringService plugin mongo" && \
+cd ../mongo && \
+cargo build --release && \
+echo "Building TriggeringService plugin napali_trigger" && \
+cd ../napali_trigger && \
+cargo build --release && \
+echo "Building TriggeringService plugin print" && \
+cd ../print && \
+cargo build --release && \
+echo "Building TriggeringService plugin threshold_trigger" && \
+cd ../threshold_trigger && \
+cargo build --release && \
 sccache --stop-server
