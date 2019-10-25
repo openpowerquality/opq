1e09a17121c31fd608fd145103e39d877a9bf3d0
anthonyjchriste <achriste@hawaii.edu>
2019-10-24 17:57:01 -1000

    Start implementing new Semi F47 plugin

--- mauka/plugins/semi_f47_plugin.py
+++ mauka/plugins/semi_f47_plugin.py
@@ -2,98 +2,91 @@
 This module contains the plugin definition for classifying voltage incidents by the Semi F47 standard.
 """
 
+import logging
 import typing
 
-import numpy
+import shapely.geometry
 
+import analysis
 import config
-import plugins.base_plugin
-from plugins.routes import Routes
-import protobuf.pb_util
-import protobuf.mauka_pb2
-import constants
 import mongo
+import plugins.base_plugin
+import protobuf.mauka_pb2
+import protobuf.pb_util
+from plugins.routes import Routes
+
+MAX_X_MS = 1_000_000
+
+SEMI_F47_VIOLATION_POLYGON = [
+    (0, 0),
+    (1, 0),
+    (1, 50),
+    (analysis.ms_to_c(200), 50),
+    (analysis.ms_to_c(200), 70),
+    (analysis.ms_to_c(500), 70),
+    (analysis.ms_to_c(500), 80),
+    (analysis.ms_to_c(10_000), 80),
+    (analysis.ms_to_c(10_000), 90),
+    (analysis.ms_to_c(MAX_X_MS), 90),
+    (analysis.ms_to_c(MAX_X_MS), 0),
+    (0, 0),
+]
+
+POLYGON = shapely.geometry.Polygon(SEMI_F47_VIOLATION_POLYGON)
 
 
-PU = 120.0
-
-
-# time in msec (1e-3 sec),
-# of one data in the array
-
-
-def viol_check(data, lvl):
+def point_in_poly(x_point: float, y_point: float) -> bool:
     """
-    viol_check function checks the data for all
-    possible instances of the SEMI_F47 violation. The
-    level is specified by the second argument lvl.
-    :param data: The data to test.
-    :param lvl: Level of violation.
-    :return: Violations.
+    Checks if the given coordinates are within the SEMI F47 violation region.
+    :param x_point: The x-coord.
+    :param y_point: The y-coord.
+    :return: True if it is, False otherwise.
     """
-    time_datum = 200.0 * 1.0 / constants.SAMPLES_PER_MILLISECOND
-
-    lvl_tym = 200 if lvl == 5 else 500 if lvl == 7 else 1000  # is in msec
-    lvl = 0.5 if lvl == 5 else 0.7 if lvl == 7 else 0.8
-
-    # pylint: disable=E1111
-    data_bool = numpy.logical_and(data >= (lvl - 0.05) * PU, data <= (lvl + 0.05) * PU)
-
-    prev = 0
-    violations = []
-    k = 0
-    prev_ind = 0
-    for k in range(0, data_bool.size):
-        if data_bool[k]:
-            prev += 1
-            if prev == 1:
-                prev_ind = k
-        else:
-            if prev * time_datum >= lvl_tym:
-                violations.append([prev_ind, k - 1])
-            prev = 0
-
-    if prev * time_datum >= lvl_tym:
-        violations.append([prev_ind, k])
-
-    return violations
+    point = shapely.geometry.Point(x_point, y_point)
+    return POLYGON.contains(point) or POLYGON.intersects(point)
 
 
-def semi_violation(mongo_client, mauka_message) -> typing.List[int]:
+def semi_violation(mongo_client: mongo.OpqMongoClient,
+                   mauka_message: protobuf.mauka_pb2.MaukaMessage) -> typing.List[int]:
     """
     Calculate semi violations.
     :param mongo_client: Mongo client for DB access.
     :param mauka_message: Mauka message to calculate violations over.
     """
-    event_id = mauka_message.payload.event_id
-    box_id = mauka_message.payload.box_id
-    data = protobuf.pb_util.repeated_as_ndarray(mauka_message.payload.data)
-    start_time_ms = mauka_message.payload.start_timestamp_ms
-
-    # this will check if a violation ocurred or not
-    time_datum = 200.0 * 1.0 / constants.SAMPLES_PER_MILLISECOND
-    lvl = [5, 7, 8]
     incident_ids = []
-    for idx in range(3):
-        possible_violations = viol_check(data, lvl[idx])
-        if possible_violations:
-            # there are violations
-            for _, violation in enumerate(possible_violations):
-                dev = max(abs(120.0 - numpy.max(data[violation[0], violation[1]])),
-                          abs(120.0 - numpy.min(data[violation[0], violation[1]])))
-                incident_id = mongo.store_incident(
-                    event_id,
-                    box_id,
-                    start_time_ms + violation[0] * time_datum,
-                    start_time_ms + (violation[1] + 1) * time_datum,
-                    mongo.IncidentMeasurementType.VOLTAGE,
-                    dev,
-                    [mongo.IncidentClassification.SEMI_F47_VIOLATION],
-                    [],
-                    {},
-                    mongo_client)
 
-                incident_ids.append(incident_id)
+    data = protobuf.pb_util.repeated_as_ndarray(mauka_message.payload.data)
+    segments = analysis.segment_array(data)
+
+    for i, segment in enumerate(segments):
+        segment_len_c = len(segment)
+
+        if segment_len_c == 1:
+            continue
+
+        segment_len_ms = analysis.c_to_ms(segment_len_c)
+        segment_mean = segment.mean()
+
+        if point_in_poly(segment_len_c, segment_mean):
+            # New SEMI F47 violation
+            start_t = analysis.c_to_ms(sum([len(segments[x]) for x in range(0, i)]))
+            end_t = start_t + segment_len_ms
+            incident_start_timestamp_ms = mauka_message.payload.start_timestamp_ms + start_t
+            incident_end_timestamp_ms = mauka_message.payload.start_timestamp_ms + end_t
+
+            incident_id = mongo.store_incident(mauka_message.payload.event_id,
+                                               mauka_message.payload.box_id,
+                                               incident_start_timestamp_ms,
+                                               incident_end_timestamp_ms,
+                                               mongo.IncidentMeasurementType.VOLTAGE,
+                                               segment_mean - 120.0,
+                                               [mongo.IncidentClassification.SEMI_F47_VIOLATION],
+                                               [],
+                                               {},
+                                               mongo_client)
+
+            incident_ids.append(incident_id)
+
     return incident_ids
 
 
@@ -105,8 +98,9 @@ class SemiF47Plugin(plugins.base_plugin.MaukaPlugin):
     def __init__(self, conf: config.MaukaConfig, exit_event):
         super().__init__(conf, [Routes.rms_windowed_voltage], "SemiF47Plugin", exit_event)
 
-    def on_message(self, topic, mauka_message):
+    def on_message(self, topic: str, mauka_message: protobuf.mauka_pb2.MaukaMessage):
         if protobuf.pb_util.is_payload(mauka_message, protobuf.mauka_pb2.VOLTAGE_RMS_WINDOWED):
+            self.debug("Recv windowed voltage")
             incident_ids = semi_violation(self.mongo_client, mauka_message)
             for incident_id in incident_ids:
                 # Produce a message to the GC
