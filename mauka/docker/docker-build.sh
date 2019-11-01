980bcf89781d43e81bbd2a65623e179a59daa48c
opq <achriste@hawaii.edu>
2019-10-31 16:03:34 -1000

    Update docker files for Mauka.

--- mauka/docker/docker-build.sh
+++ mauka/docker/docker-build.sh
@@ -3,7 +3,7 @@
 echo "Building mauka..."
 mkdir -p build
 cd ../..
-rsync -av --progress mauka mauka/docker/build --exclude mauka/docker
+rsync -av --progress mauka mauka/docker/build --exclude mauka/docker --exclude mauka/mauka_native/target
 cd mauka/docker
 docker build -t mauka .
 rm -rf build
