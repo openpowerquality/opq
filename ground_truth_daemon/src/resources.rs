4915d6771efbd30fb7be573c68853349f4a8a522
anthonyjchriste <achriste@hawaii.edu>
2019-12-20 08:09:14 -1000

    Update ground truth to pull from all meters.

--- ground_truth_daemon/src/resources.rs
+++ ground_truth_daemon/src/resources.rs
@@ -19,35 +19,35 @@ impl Meters {
     pub fn feature_ids(&self, feature_str: &String) -> Vec<String> {
         let mut resource_ids: Vec<String> = Vec::new();
 
-        let whitelist: Vec<&str> = vec![
-            "POST_MAIN_1",
-            "POST_MAIN_2",
-            "HAMILTON_LIB_PH_III_CH_1_MTR",
-            "HAMILTON_LIB_PH_III_CH_2_MTR",
-            "HAMILTON_LIB_PH_III_CH_3_MTR",
-            "HAMILTON_LIB_PH_III_MAIN_1_MTR",
-            "HAMILTON_LIB_PH_III_MAIN_2_MTR",
-            "HAMILTON_LIB_PH_III_MCC_AC1_MTR",
-            "HAMILTON_LIB_PH_III_MCC_AC2_MTR",
-            "KELLER_HALL_MAIN_MTR",
-            "MARINE_SCIENCE_MAIN_A_MTR",
-            "MARINE_SCIENCE_MAIN_B_MTR",
-            "MARINE_SCIENCE_MCC_MTR",
-            "AG_ENGINEERING_MAIN_MTR",
-            "AG_ENGINEERING_MCC_MTR",
-            "LAW_LIB_MAIN_MTR",
-            "KENNEDY_THEATRE_MAIN_MTR",
-        ];
+        //        let whitelist: Vec<&str> = vec![
+        //            "POST_MAIN_1",
+        //            "POST_MAIN_2",
+        //            "HAMILTON_LIB_PH_III_CH_1_MTR",
+        //            "HAMILTON_LIB_PH_III_CH_2_MTR",
+        //            "HAMILTON_LIB_PH_III_CH_3_MTR",
+        //            "HAMILTON_LIB_PH_III_MAIN_1_MTR",
+        //            "HAMILTON_LIB_PH_III_MAIN_2_MTR",
+        //            "HAMILTON_LIB_PH_III_MCC_AC1_MTR",
+        //            "HAMILTON_LIB_PH_III_MCC_AC2_MTR",
+        //            "KELLER_HALL_MAIN_MTR",
+        //            "MARINE_SCIENCE_MAIN_A_MTR",
+        //            "MARINE_SCIENCE_MAIN_B_MTR",
+        //            "MARINE_SCIENCE_MCC_MTR",
+        //            "AG_ENGINEERING_MAIN_MTR",
+        //            "AG_ENGINEERING_MCC_MTR",
+        //            "LAW_LIB_MAIN_MTR",
+        //            "KENNEDY_THEATRE_MAIN_MTR",
+        //        ];
+        //
+        //        let whitelist: HashSet<&str> = HashSet::from_iter(whitelist.iter().cloned());
+        //
+        //        let whitelisted_meters: Vec<&Meter> = self
+        //            .meters
+        //            .iter()
+        //            .filter(|meter| whitelist.contains(&meter.meter_name[..]))
+        //            .collect();
 
-        let whitelist: HashSet<&str> = HashSet::from_iter(whitelist.iter().cloned());
-
-        let whitelisted_meters: Vec<&Meter> = self
-            .meters
-            .iter()
-            .filter(|meter| whitelist.contains(&meter.meter_name[..]))
-            .collect();
-
-        for meter in whitelisted_meters {
+        for meter in &self.meters {
             for feature in &meter.features {
                 if feature.name == *feature_str {
                     resource_ids.push(feature.tag_id.clone());
